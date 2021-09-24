cdafaa4872b199c358b6eee2688a7fbd565ac3b2
ansony <ansonyhuallpam@gmail.com>
2021-09-24 10:54:23 -0500

    rol de guardias

--- src/app/rol/rol-routing.module.ts
+++ src/app/rol/rol-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, Component } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { RolGuardiaComponent } from "./rol-guardia/rol-guardia.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "rolGuardia",
+    component: RolGuardiaComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RolRoutingModule { }
+export class RolRoutingModule {}
--- src/app/rol/rol.module.ts
+++ src/app/rol/rol.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { RolRoutingModule } from './rol-routing.module';
-
-
+import { RolRoutingModule } from "./rol-routing.module";
+import { RolGuardiaComponent } from "./rol-guardia/rol-guardia.component";
+import { PrimeModule } from "../shared/prime/prime.module";
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    RolRoutingModule
-  ]
+  declarations: [RolGuardiaComponent],
+  imports: [CommonModule, RolRoutingModule, PrimeModule],
 })
-export class RolModule { }
+export class RolModule {}
