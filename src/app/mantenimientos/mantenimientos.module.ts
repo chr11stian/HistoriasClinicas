faf38a20f2415f175f18d65fce4a3036b5f4981c
Jimmy <yimitopc@gmail.com>
2021-09-30 15:42:03 -0500

    error

--- src/app/mantenimientos/mantenimientos.module.ts
+++ src/app/mantenimientos/mantenimientos.module.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TipoPersonalComponent } from "./component/tipo-personal/tipo-personal.component";
@@ -15,22 +15,7 @@ import { MultiSelectModule } from "primeng/multiselect";
 import { EspecialidadComponent } from "./component/especialidad/especialidad.component";
 import { TipoTurnoComponent } from "./component/tipo-turno/tipo-turno.component";
 import { TipoTurnoModalComponent } from "./component/tipo-turno-modal/tipo-turno-modal.component";
-=======
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {TipoPersonalComponent} from "./component/tipo-personal/tipo-personal.component";
-import {CoreModule} from "../core/core.module";
-import {MantenimentosRoutingModule} from "./mantenimentos-routing.module";
-import {PrimeModule} from "../shared/prime/prime.module";
-import {TipoPersonalModalComponent} from './component/tipo-personal-modal/tipo-personal-modal.component';
-import {UbicacionComponent} from "./component/ubicacion/ubicacion.component";
-import {ToolbarModule} from "primeng/toolbar";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ColegioProfesionalComponent} from "./component/colegio-profesional/colegio-profesional.component";
-import {TreeSelectModule} from "primeng/treeselect";
-import {MultiSelectModule} from "primeng/multiselect";
-import {EspecialidadComponent} from './component/especialidad/especialidad.component';
->>>>>>> jimmy
+
 
 @NgModule({
   declarations: [
