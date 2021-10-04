7d0e801482e260d57b85bf52ac7dd7b04de42a8c
Jimmy <yimitopc@gmail.com>
2021-10-04 11:25:41 -0500

    nombre comercial UPS JPC

---
+++ src/app/mantenimientos/component/nombre-comercial-ups/nombre-comercial-ups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nombre-comercial-ups',
+  templateUrl: './nombre-comercial-ups.component.html',
+  styleUrls: ['./nombre-comercial-ups.component.css']
+})
+export class NombreComercialUPSComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
