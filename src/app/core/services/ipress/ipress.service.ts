dfed4ca5f499a45584bf39e91e01ee81dcaa21e7
leticia <160893@unsaac.edu.pe>
2021-10-28 21:26:55 -0500

    Agregando mantenimiento de ipress junto a ambientes y turnos

--- src/app/core/services/ipress/ipress.service.ts
+++ src/app/core/services/ipress/ipress.service.ts
@@ -33,11 +33,25 @@ export class IpressService {
     createJurisdiccionIpress(idIpress,req){
         return this.http.put<any>(`${this.base_url}/${this.bd}/api/ipress/agregarjurisdiccion/${idIpress}`,req)
     }
-    editJurisdiccionIpress(idIpress,req){
-        return this.http.put<any>(`${this.base_url}/${this.bd}/api/ipress/actualizarjurisdiccion/${idIpress}`,req)
-    }
     deleteJurisdiccionIpress(idIpress,req){
-        return this.http.delete<any>(`${this.base_url}/${this.bd}/api/ipress/eliminarjurisdiccion/${idIpress}`,req)
+        return this.http.post<any>(`${this.base_url}/${this.bd}/api/ipress/eliminarjurisdiccion/${idIpress}`,req)
     }
-    
+    createAmbienteIpress(idIpress,req){
+        return this.http.put<any>(`${this.base_url}/${this.bd}/api/ipress/agregarambiente/${idIpress}`,req)
+    }
+    editAmbienteIpress(idIpress,req){
+        return this.http.put<any>(`${this.base_url}/${this.bd}/api/ipress/actualizarambiente/${idIpress}`,req)
+    }
+    deleteAmbienteIpress(idIpress,codAmbiente){
+        return this.http.delete<any>(`${this.base_url}/${this.bd}/api/ipress//eliminarambiente/${idIpress}/${codAmbiente}`)
+    }
+    createTurnoIpress(idIpress,req){
+        return this.http.put<any>(`${this.base_url}/${this.bd}/api/ipress/agregarturno/${idIpress}`,req)
+    }
+    editTurnoIpress(idIpress,req){
+        return this.http.put<any>(`${this.base_url}/${this.bd}/api/ipress/actualizarturno/${idIpress}`,req)
+    }
+    deleteTurnoIpress(idIpress,abreviatura){
+        return this.http.delete<any>(`${this.base_url}/${this.bd}/api/ipress/eliminarturno/${idIpress}/${abreviatura}`)
+    }
 }
