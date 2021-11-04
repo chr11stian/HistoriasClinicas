9a02b0683e758e22e9c844b24f20447207e5cea8
christian <jc.yunguridata@gmail.com>
2021-11-04 10:20:43 -0500

    cred

---
+++ src/app/cred/cred.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CredRoutingModule } from './cred-routing.module';
+import { DatosGeneralesComponent } from './datos-generales/datos-generales.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { AntecendentesComponent } from './antecendentes/antecendentes.component';
+import { PlanAtencionIntegralComponent } from './plan-atencion-integral/plan-atencion-integral.component';
+import { EvaluacionGeneralComponent } from './evaluacion-general/evaluacion-general.component';
+import { TestDesarrolloComponent } from './test-desarrollo/test-desarrollo.component';
+
+
+@NgModule({
+  declarations: [
+    DatosGeneralesComponent,
+    CabeceraComponent,
+    AntecendentesComponent,
+    PlanAtencionIntegralComponent,
+    EvaluacionGeneralComponent,
+    TestDesarrolloComponent
+  ],
+  imports: [
+    CommonModule,
+    CredRoutingModule
+  ]
+})
+export class CredModule { }
