a5b7b91b40bd7887df5d9dbb4affcf49f6134ad6
DanielMos1817 <37486230+DanielMos1817@users.noreply.github.com>
2021-11-05 10:41:20 -0500

    init graph

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GraphicComponent } from './components/graphic/graphic.component';
+
+
+
+@NgModule({
+  declarations: [
+    GraphicComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
