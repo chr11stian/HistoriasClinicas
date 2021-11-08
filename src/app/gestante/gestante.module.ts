1eeee5b2df8278f1ce8c2e09f303af01c8701c44
Jimmy <yimitopc@gmail.com>
2021-11-08 17:26:37 -0500

    Filiacion y antecendentes

--- src/app/gestante/gestante.module.ts
+++ src/app/gestante/gestante.module.ts
@@ -1,33 +1,46 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PrimeModule } from "src/app/shared/prime/prime.module";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PrimeModule} from "src/app/shared/prime/prime.module";
 
-import { GestanteRoutingModule } from './gestante-routing.module';
-import { FiliacionAntecedentesComponent } from './component/filiacion-antecedentes/filiacion-antecedentes.component';
-import { StepGeneralComponent } from './component/step-general/step-general.component';
-import { DatosBasalesComponent } from './component/datos-basales/datos-basales.component';
-import { AtencionesComponent } from './component/atenciones/atenciones.component';
-import { PartosComponent } from './component/partos/partos.component';
-import { RecienNacidoComponent } from './component/recien-nacido/recien-nacido.component';
-import { PuerperioComponent } from './component/puerperio/puerperio.component';
-import { DatosGeneralesObtetriciaComponent } from './component/datos-generales-obtetricia/datos-generales-obtetricia.component';
+import {GestanteRoutingModule} from './gestante-routing.module';
+import {StepGeneralComponent} from './component/step-general/step-general.component';
+import {DatosBasalesComponent} from './component/datos-basales/datos-basales.component';
+import {AtencionesComponent} from './component/atenciones/atenciones.component';
+import {PartosComponent} from './component/partos/partos.component';
+import {RecienNacidoComponent} from './component/recien-nacido/recien-nacido.component';
+import {PuerperioComponent} from './component/puerperio/puerperio.component';
+import {DatosGeneralesObtetriciaComponent} from "./component/filiacion-antecedentes/datos-generales-obtetricia.component";
+import {ToolbarModule} from "primeng/toolbar";
+import {PanelModule} from "primeng/panel";
+import {FieldsetModule} from "primeng/fieldset";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CheckboxModule} from "primeng/checkbox";
 
 
 @NgModule({
-  declarations: [
-    FiliacionAntecedentesComponent,
-    StepGeneralComponent,
-    DatosBasalesComponent,
-    AtencionesComponent,
-    PartosComponent,
-    RecienNacidoComponent,
-    PuerperioComponent,
-    DatosGeneralesObtetriciaComponent
-  ],
-  imports: [
-    PrimeModule,
-    CommonModule,
-    GestanteRoutingModule
-  ]
+    declarations: [
+        StepGeneralComponent,
+        DatosBasalesComponent,
+        AtencionesComponent,
+        PartosComponent,
+        RecienNacidoComponent,
+        PuerperioComponent,
+        DatosGeneralesObtetriciaComponent
+    ],
+
+    exports: [DatosGeneralesObtetriciaComponent],
+
+    imports: [
+        PrimeModule,
+        CommonModule,
+        GestanteRoutingModule,
+        ToolbarModule,
+        PanelModule,
+        FieldsetModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CheckboxModule
+    ]
 })
-export class GestanteModule { }
+export class GestanteModule {
+}
