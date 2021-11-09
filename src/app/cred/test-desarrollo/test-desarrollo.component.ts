22f49e2edb73e6c99873e8f7e034e1df3c201fca
Fernando Callasaca <fernandocallasaca@outlook.com>
2021-11-09 13:02:23 -0500

    cabeceras finish

--- src/app/cred/test-desarrollo/test-desarrollo.component.ts
+++ src/app/cred/test-desarrollo/test-desarrollo.component.ts
@@ -12,11 +12,8 @@ export class TestDesarrolloComponent implements OnInit {
 
   ngOnInit(): void {
     this.sales = [
-      { brand: 'Apple', lastYearSale: '51%', thisYearSale: '40%', lastYearProfit: '$54,406.00', thisYearProfit: '$43,342' },
-      { brand: 'Samsung', lastYearSale: '83%', thisYearSale: '96%', lastYearProfit: '$423,132', thisYearProfit: '$312,122' },
-      { brand: 'Microsoft', lastYearSale: '38%', thisYearSale: '5%', lastYearProfit: '$12,321', thisYearProfit: '$8,500' },
-      { brand: 'Philips', lastYearSale: '49%', thisYearSale: '22%', lastYearProfit: '$745,232', thisYearProfit: '$650,323,' },
-      { brand: 'Song', lastYearSale: '17%', thisYearSale: '79%', lastYearProfit: '$643,242', thisYearProfit: '500,332' },
+      { letter: 'A', }, { letter: 'B', }, { letter: 'C', }, { letter: 'D', }, { letter: 'E', }, { letter: 'F', }, { letter: 'G', },
+      { letter: 'H', },{ letter: 'I', },{ letter: 'J', },{ letter: 'K', },{ letter: 'L', },
   ];
   }
 
