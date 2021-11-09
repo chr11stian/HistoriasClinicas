4bc2ab6a3079ac522097edd1429a730e9fb6e4b9
Fernando Callasaca <fernandocallasaca@outlook.com>
2021-11-09 15:04:02 -0500

    imagenes y primera imagen en test

--- src/app/cred/test-desarrollo/test-desarrollo.component.ts
+++ src/app/cred/test-desarrollo/test-desarrollo.component.ts
@@ -12,9 +12,23 @@ export class TestDesarrolloComponent implements OnInit {
 
   ngOnInit(): void {
     this.sales = [
-      { letter: 'A', }, { letter: 'B', }, { letter: 'C', }, { letter: 'D', }, { letter: 'E', }, { letter: 'F', }, { letter: 'G', },
-      { letter: 'H', },{ letter: 'I', },{ letter: 'J', },{ letter: 'K', },{ letter: 'L', },
+      { letter: 'A', img_1mes: '../../../assets/images/tdd_imagenes/1_mes/1-1mes-01.svg'}, 
+      { letter: 'B', },
+      { letter: 'C', },
+      { letter: 'D', },
+      { letter: 'E', },
+      { letter: 'F', },
+      { letter: 'G', },
+      { letter: 'H', },
+      { letter: 'I', },
+      { letter: 'J', },
+      { letter: 'K', },
+      { letter: 'L', },
   ];
+  }
+
+  cambiarEstado(){
+
   }
 
 }
