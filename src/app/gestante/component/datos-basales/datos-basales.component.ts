aa60c61fc1b618aafbd17618ea0133b5f6dde0e2
jonathan <jona.ml1011@gmail.com>
2021-11-11 16:18:02 -0500

    controlName de componentes

--- src/app/gestante/component/datos-basales/datos-basales.component.ts
+++ src/app/gestante/component/datos-basales/datos-basales.component.ts
@@ -13,7 +13,7 @@ export class DatosBasalesComponent implements OnInit {
     { label: 'SI', value: 'SI' },
     { label: 'NO', value: 'NO' }
   ];
-  city:any;
+  city: any;
 
   constructor(
     private fb: FormBuilder,
@@ -35,22 +35,61 @@ export class DatosBasalesComponent implements OnInit {
       a: new FormControl(''),
       drogas: new FormControl(''),
       date: new FormControl(''),
-      rdo: '',
-      console:'',
+      rdo: new FormControl(''),
+      console: new FormControl(''),
       aplica: new FormControl(''),
-      noAplica: new FormControl(''),
-      tipoSangre: new FormControl(''),
+      sinDosis: new FormControl(''),
+      dosisNoAplica: new FormControl(''),
+      cigarrillosDia: new FormControl(''),
+      tipoSangreGrupo: new FormControl(''),
       rh: new FormControl(''),
+      duda: new FormControl(''),
       clinico: new FormControl(''),
       mamas: new FormControl(''),
       cuelloUter: new FormControl(''),
       pelvis: new FormControl(''),
       odont1: new FormControl(''),
       odont2: new FormControl(''),
+      vdrl1: new FormControl(''),
+      vdrl2: new FormControl(''),
+      tpha: new FormControl(''),
+      dateVdrl2: new FormControl(''),
+      dateVih2: new FormControl(''),
+      vih1: new FormControl(''),
+      vih2: new FormControl(''),
+      hepatitis: new FormControl(''),
+      elisa1: new FormControl(''),
+      elisa2: new FormControl(''),
+      glicemia1: new FormControl(''),
+      glicemia2: new FormControl(''),
+      glucosa: new FormControl(''),
+      orina1: new FormControl(''),
+      orina2: new FormControl(''),
+      orina3: new FormControl(''),
+      bacteriuria: new FormControl(''),
+      urocultivo: new FormControl(''),
+      esputo: new FormControl(''),
+      western: new FormControl(''),
+      thlv1: new FormControl(''),
+      torch: new FormControl(''),
+      gotaGruesa: new FormControl(''),
+      proteinuriaCuanti: new FormControl(''),
+      proteinuriaCuali: new FormControl(''),
+      secrecionVag: new FormControl(''),
+      pap: new FormControl(''),
+      ivaa: new FormControl(''),
+      dateFUM: new FormControl(''),
+      dateProbableParto: new FormControl(''),
+      ecografia1: new FormControl(''),
+      dateEco1: new FormControl(''),
+      ecografia2: new FormControl(''),
+      dateEco2: new FormControl(''),
+      ecografia3: new FormControl(''),
+      dateEco3: new FormControl(''),
     });
   }
 
-  fnCheckbox(value){
+  fnCheckbox(value) {
     console.log(value);
   }
 }
