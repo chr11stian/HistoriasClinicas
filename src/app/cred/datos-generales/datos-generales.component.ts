002888ea482bcd4411f21391259934a444033c17
christian <jc.yunguridata@gmail.com>
2021-11-11 12:40:10 -0500

    datos-generales-cred

--- src/app/cred/datos-generales/datos-generales.component.ts
+++ src/app/cred/datos-generales/datos-generales.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { FormBuilder, Validators, FormGroup } from "@angular/forms"
 
 @Component({
-  selector: 'app-datos-generales',
-  templateUrl: './datos-generales.component.html',
-  styleUrls: ['./datos-generales.component.css']
+  selector: "app-datos-generales",
+  templateUrl: "./datos-generales.component.html",
+  styleUrls: ["./datos-generales.component.css"],
 })
 export class DatosGeneralesComponent implements OnInit {
+  form: FormGroup
+  stateOptions: any[]
+  stateOptions1: any[]
+  stateOptions2: any[]
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.stateOptions = [
+      { label: "F", value: 1 },
+      { label: "M", value: 2 },
+    ]
+    this.stateOptions1 = [
+      { label: "G.S", value: 1 },
+      { label: "NO", value: 2 },
+    ]
+    this.stateOptions2 = [
+      { label: "RH", value: 1 },
+      { label: "NO", value: 2 },
+    ]
   }
 
+  ngOnInit(): void {}
 }
