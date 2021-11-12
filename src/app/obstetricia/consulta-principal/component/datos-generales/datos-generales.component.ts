6d05a2c4820623186f271f3f7e9b45b36452e6a7
Jimmy <yimitopc@gmail.com>
2021-11-12 09:37:27 -0500

    datos generales

--- src/app/obstetricia/consulta-principal/component/datos-generales/datos-generales.component.ts
+++ src/app/obstetricia/consulta-principal/component/datos-generales/datos-generales.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-datos-generales',
-  templateUrl: './datos-generales.component.html',
-  styleUrls: ['./datos-generales.component.css']
+    selector: 'app-datos-generales',
+    templateUrl: './datos-generales.component.html',
+    styleUrls: ['./datos-generales.component.css']
 })
 export class DatosGeneralesComponent implements OnInit {
+    formDatos_Generales: FormGroup;
 
-  constructor() { }
+    constructor(private form: FormBuilder,) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.buildForm();
+    }
 
+    buildForm() {
+        this.formDatos_Generales = this.form.group({
+            // apPaterno: new FormControl(''),
+            // ApMaterno: new FormControl(''),
+            // nombres: new FormControl(''),
+            aplica: new FormControl(''),
+            // referencia: new FormControl(''),
+            // partoVaginal: new FormControl(''),
+        })
+    }
 }
