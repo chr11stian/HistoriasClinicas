e2013f689c2cd8528f0c72a717f3d55b9c6e5c94
jimmy <yimitopc@gmail.com>
2021-11-13 13:54:06 -0500

    reestructurado

--- src/app/obstetricia/obstetricia.module.ts
+++ src/app/obstetricia/obstetricia.module.ts
@@ -1,13 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {DatosGeneralesComponent} from './consulta-principal/component/datos-generales/datos-generales.component';
 import {PrimeModule} from "../shared/prime/prime.module";
 import {ObtetriciaRoutingModule} from "./obstetricia-routing.module";
-import {StepGeneral_consultaComponent} from "./consulta-principal/component/step-general/step-general-consulta.component";
-import {InterrogatorioComponent} from './consulta-principal/component/interrogatorio/interrogatorio.component';
-import {GiagnosticosComponent} from './consulta-principal/component/giagnosticos/giagnosticos.component';
-import {TratamientoComponent} from './consulta-principal/component/tratamiento/tratamiento.component';
-import {ResultadosComponent} from './consulta-principal/component/resultados/resultados.component';
+
 import {AtencionesComponent} from "./gestante/component/atenciones/atenciones.component";
 import {DatosBasalesComponent} from "./gestante/component/datos-basales/datos-basales.component";
 import {DatosGeneralesObtetriciaComponent} from "./gestante/component/filiacion-antecedentes/datos-generales-obtetricia.component";
@@ -15,9 +10,7 @@ import {PartosComponent} from "./gestante/component/partos/partos.component";
 import {PuerperioComponent} from "./gestante/component/puerperio/puerperio.component";
 import {RecienNacidoComponent} from "./gestante/component/recien-nacido/recien-nacido.component";
 import {StepGeneralComponent} from "./gestante/component/step-general/step-general.component";
-import {ReactiveFormsModule} from "@angular/forms";
-import {ToolbarModule} from "primeng/toolbar";
-import {FieldsetModule} from "primeng/fieldset";
+
 import {PaginatorModule} from "primeng/paginator";
 import {DatosGeneralesPartoComponent} from './plan-de-parto/component/datos-generales-parto/datos-generales-parto.component';
 import {StepGeneralPartoComponent} from "./plan-de-parto/component/step-general/step-general-parto.component";
@@ -25,31 +18,15 @@ import {IntervaloPartoComponent} from './plan-de-parto/component/intervalo-parto
 import {NecesidadesPartoComponent} from './plan-de-parto/component/necesidades-parto/necesidades-parto.component';
 import {SPeligroPartoComponent} from './plan-de-parto/component/s-peligro-parto/s-peligro-parto.component';
 import {DialogAtencionComponent} from './gestante/component/atenciones/dialog-atencion/dialog-atencion.component';
+import {ObstetriciaComponent} from './obstetricia.component';
+import {ConsultaPrincipalModule} from "./consulta-principal/consulta-principal.module";
+import {GestanteModule} from "./gestante/gestante.module";
+import {PlanDePartoModule} from "./plan-de-parto/plan-de-parto.module";
 
 
 @NgModule({
     declarations: [
-        DatosGeneralesComponent,
-        StepGeneral_consultaComponent,
-        InterrogatorioComponent,
-        GiagnosticosComponent,
-        TratamientoComponent,
-        ResultadosComponent,
-
-        AtencionesComponent,
-        DatosBasalesComponent,
-        DatosGeneralesObtetriciaComponent,
-        PartosComponent,
-        PuerperioComponent,
-        RecienNacidoComponent,
-        StepGeneralComponent,
-
-        DatosGeneralesPartoComponent,
-        StepGeneralPartoComponent,
-        IntervaloPartoComponent,
-        NecesidadesPartoComponent,
-        SPeligroPartoComponent,
-        DialogAtencionComponent
+        ObstetriciaComponent,
 
     ],
     exports: [],
@@ -59,6 +36,9 @@ import {DialogAtencionComponent} from './gestante/component/atenciones/dialog-at
         PrimeModule,
         PaginatorModule,
 
+        ConsultaPrincipalModule,
+        GestanteModule,
+        PlanDePartoModule
     ]
 })
 export class ObstetriciaModule {
