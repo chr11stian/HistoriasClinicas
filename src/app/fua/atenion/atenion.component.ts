3bf2434b986579bad5aa56ea8ac16d1d219fe850
madaihq2020 <59493896+madaihq2020@users.noreply.github.com>
2021-11-15 17:31:59 -0500

    atenciones fua

--- src/app/fua/atenion/atenion.component.ts
+++ src/app/fua/atenion/atenion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-atenion',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atenion.component.css']
 })
 export class AtenionComponent implements OnInit {
+    formDeLaAtencion: any;
 
-  constructor() { }
+  constructor(private form: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+  buildForm() {
+    this.formDeLaAtencion = this.form.group({
+      // apPaterno: new FormControl(''),
+      // // ApMaterno: new FormControl(''),
+      // // nombres: new FormControl(''),
+      // aplica: new FormControl(''),
+      // referencia: new FormControl(''),
+    })
   }
 
 }
