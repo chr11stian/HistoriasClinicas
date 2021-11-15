a1f6e65550211a7f106d477f74b88958f7edf3a4
Jimmy <yimitopc@gmail.com>
2021-11-15 10:40:56 -0500

    fua

---
+++ src/app/fua/atenion/atenion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atenion',
+  templateUrl: './atenion.component.html',
+  styleUrls: ['./atenion.component.css']
+})
+export class AtenionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
