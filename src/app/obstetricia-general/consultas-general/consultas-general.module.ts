1aea7728749da643a69beaac2d3508bbdf949b89
Jimmy <yimitopc@gmail.com>
2021-11-16 12:32:16 -0500

    aa

--- src/app/obstetricia-general/consultas-general/consultas-general.module.ts
+++ src/app/obstetricia-general/consultas-general/consultas-general.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {NuevaConsultaComponent} from "./nueva-consulta/nueva-consulta.component";
 import {ConsultasGeneralRoutingModule} from "./consultas-general-routing.module";
 import {PrimeModule} from "../../shared/prime/prime.module";
 import {PaginatorModule} from "primeng/paginator";
@@ -9,7 +8,6 @@ import {ConsultasGeneralComponent} from "./consultas-general.component";
 
 @NgModule({
     declarations: [
-        NuevaConsultaComponent,
         ConsultasGeneralComponent,
     ],
     imports: [
