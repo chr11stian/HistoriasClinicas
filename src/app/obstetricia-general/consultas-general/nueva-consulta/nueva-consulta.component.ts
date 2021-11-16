1b373856d0099df9d19048a4d11a3e878816b9f8
leticia <160893@unsaac.edu.pe>
2021-11-16 12:12:45 -0500

    Agregando consulta de gestante

--- src/app/obstetricia-general/consultas-general/nueva-consulta/nueva-consulta.component.ts
+++ src/app/obstetricia-general/consultas-general/nueva-consulta/nueva-consulta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-nueva-consulta',
@@ -6,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nueva-consulta.component.css']
 })
 export class NuevaConsultaComponent implements OnInit {
-
-  constructor() { }
-
+  form: FormGroup;
+  todosEgresosDelRN: any[];
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.buildForm();
+    this.todosEgresosDelRN = [{
+      tratamientos: "Tomar paracetamol 500mg",
+      diagnostico: "Presenta sintomas de alerta",
+    },
+    {
+      tratamientos: "Mantener Reposo",
+      diagnostico: "Presenta sintomas leves",
+    },
+    {
+      tratamientos: "Tomar paracetamol 500mg",
+      diagnostico: "Presenta sintomas de peligro",
+    },
+    {
+      tratamientos: "Tomar paracetamol 500mg",
+      diagnostico: "Presenta sintomas leves",
+    }]
+   }
+   buildForm() {
+    this.form = this.formBuilder.group({
+      sexo: [''],
+      pesoRN: [''],
+    })
+  }
   ngOnInit(): void {
   }
 
