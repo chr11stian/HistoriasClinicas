cb2312e6139e0a6a2538e9c2875dd583a48d9eee
madaihq2020 <59493896+madaihq2020@users.noreply.github.com>
2021-11-18 17:25:22 -0500

    ab

--- src/app/obstetricia-general/gestante/atencion/atencion.component.ts
+++ src/app/obstetricia-general/gestante/atencion/atencion.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atencion.component.css']
 })
 export class AtencionComponent implements OnInit {
+  titleBienvenida: string = "¡Iniciemos!";
+  contentBienvenida:string = "Puedes empezar seleccionando Plan de Atención Integral Cada una de las secciones contiene todo lo que necesitas para completar su primera consulta.";
+  titlePrimeraFase:string="¡Continuamos!";
+  contentPrimeraFase: string ="";
+  titleSegundaFase: string="Por último";
+  contentSegundaFase:string="";
 
   constructor() {}
 
