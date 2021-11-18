6431fae44982af6b800279c3c14b0e11c482fc73
ansony <ansonyhuallpam@gmail.com>
2021-11-18 17:24:57 -0500

    fechas parto aborto y controls

--- src/app/obstetricia-general/citas/atencion/plan-atencion-integral/component/partos/partos.component.ts
+++ src/app/obstetricia-general/citas/atencion/plan-atencion-integral/component/partos/partos.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 import { NgModule } from "@angular/core";
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from "@angular/forms";
 import { debounceTime } from "rxjs/operators";
+import {PartoAbortoService} from "../../services/parto-aborto/parto-aborto.service";
 
 @Component({
   selector: "app-partos",
@@ -16,18 +18,24 @@ import { debounceTime } from "rxjs/operators";
 export class PartosComponent implements OnInit {
   treeOptionsOptions: any[];
   twoOptions: any[];
+  TFOptions:any[];
   myGroup: FormGroup;
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder,
+              private partoAbortoService:PartoAbortoService) {
     this.twoOptions = [
-      { label: "Si", value: "Si" },
-      { label: "No", value: "No" },
+      { label: "Si", value: "si" },
+      { label: "No", value: "no" },
     ];
     this.treeOptionsOptions = [
       { label: "Si", value: "si" },
       { label: "No", value: "no" },
-      { label: "No Apli.", value: "no aplica" },
+      { label: ".", value: "no aplica" },
     ];
+    this.TFOptions=[
+      {label :"Si",value:true},
+      {label:"No",value:false}
+    ]
     this.buildForm();
   }
   buildForm() {
@@ -35,6 +43,7 @@ export class PartosComponent implements OnInit {
       hcmp: new FormControl("", [Validators.required]),
       pc: new FormControl("", [Validators.required]),
       orden: new FormControl("", Validators.required),
+      ingresoEstablecimientoPartoFecha: new FormControl("", Validators.required),
       referenciaIngreso: new FormControl("", Validators.required),
       pulsoMaterno: new FormControl("", Validators.required),
       presionArterial: new FormControl("", Validators.required),
@@ -44,7 +53,7 @@ export class PartosComponent implements OnInit {
       eg: new FormControl("", Validators.required),
       situacion: new FormControl("", Validators.required),
       posicion: new FormControl("", Validators.required),
-      tamañoFetalAcorde: new FormControl("", Validators.required),
+      tamanoFetalAcorde: new FormControl("", Validators.required),
       inicio: new FormControl("", Validators.required),
       dilatacion: new FormControl("", Validators.required),
       presentacion: new FormControl("", Validators.required),
@@ -68,6 +77,7 @@ export class PartosComponent implements OnInit {
       cesaria: new FormControl("", Validators.required),
       aborto: new FormControl("", Validators.required),
       // cuarta fila
+      terminacionFecha: new FormControl("", Validators.required),
       terminacion: new FormControl("", Validators.required),
       desgarros: new FormControl("", Validators.required),
       posicionGestante: new FormControl("", Validators.required),
@@ -100,7 +110,109 @@ export class PartosComponent implements OnInit {
   // ngDoCheck(): void {
   //   this.mostrarDatos();
   // }
+  getFC(control: string): AbstractControl {
+    return this.myGroup.get(control);
+  }
   save() {
-    console.log(this.myGroup.value);
+    const partoAbortoInput:any={
+      estado:{
+        hcmp:this.getFC("hcmp").value,
+        productoConcepcion:this.getFC("pc").value,
+        orden:this.getFC("orden").value
+      },
+      ingresoEstablecimientoParto:{
+        fechaIngreso:this.getFechaHora(this.getFC("ingresoEstablecimientoPartoFecha").value),
+        referenciaIngreso:this.getFC("referenciaIngreso").value,
+        pulsoMaterno:this.getFC("pulsoMaterno").value,
+        presionArterial:this.getFC("presionArterial").value,
+        frecuenciaRespiratoria:this.getFC("frecuenciaRespiratoria").value,
+        temperatura:this.getFC("temperatura").value,
+        peso:this.getFC("peso").value,
+        eg:this.getFC("eg").value,
+        situacion:this.getFC("situacion").value,
+        presentacion:this.getFC("presentacion").value,
+        posicion:this.getFC("posicion").value,
+        alturaUterina:this.getFC("alturaUterina").value,
+        tamFetalAcorde:this.getFC("tamanoFetalAcorde").value,
+        fcf:this.getFC("fcf").value,
+        inicio:this.getFC("inicio").value,
+        dilatacion:this.getFC("dilatacion").value,
+        membranas:this.getFC("menbranas").value,
+        liquidoAmoniaco:this.getFC("liquidoAmniotico").value,
+        fechaRuptura:this.getFechaHora(this.getFC("fechaRuptura").value)
+        //  fechaRuptura:"2020-05-18 15:15:00"
+      },
+      signoSintomaAlarma:[
+        {
+          nombre:"Anasarca",
+          valor:this.getFC("anasarca").value
+        },
+        {
+          nombre:"Cianosis",
+          valor:this.getFC("cianosis").value
+        },
+        {
+          nombre:"Escotomas",
+          valor:this.getFC("escotomas").value
+        },
+        {
+          nombre:"Petequias",
+          valor:this.getFC("petequies").value
+        }
+      ],
+      coticoidesAntenatales:{
+        nombre:this.getFC("corticoidesAntenatales").value,
+        semanaInicio:this.getFC("semanaInicio").value
+      },
+      tipoProcedimiento:{
+        cesaria:this.getFC("cesaria").value,
+        aborto:this.getFC("aborto").value
+      },
+      procedimientoParto:{
+        medicacion:["mediacion 1"],
+        medicamentos:["medicamento 1"]
+      },
+      indicacionPrincipalParto:this.getFC("indicacionPrincipalPartoOperatorio").value,
+      hubo:this.getFC("indicacionPrincipalHubo").value,
+      terminacion:{
+        fecha:this.getFechaHora(this.getFC("terminacionFecha").value),
+        terminacion:this.getFC("terminacion").value,
+        desgarros:this.getFC("desgarros").value,
+        posiGestante:this.getFC("posicionGestante").value,
+        duracion:this.getFC("duracion").value,
+        alumbramiento:this.getFC("alumbramiento").value,
+        partoGrama:this.getFC("partoGrama").value,
+        muerteIntraUterina:this.getFC("muerteIntrauterina").value,
+        placenta:this.getFC("placenta").value,
+        partoConAcompaniante:this.getFC("partoAconpanante").value,
+        ligaduraCordon:this.getFC("ligaduraCordon").value
+      },
+      atencion:{
+        nivel:this.getFC("nivel").value,
+        partoLegrado:this.getFC("partoLegrado").value,
+        neonato:this.getFC("neonato").value,
+        responsableAtencionParto:"sss",
+        responsableAtencionNeonato:"sss"
+      }
+    }
+    this.partoAbortoService.addPartoAborto(partoAbortoInput).subscribe((resp)=>{
+      console.log(resp)
+      console.log(partoAbortoInput)
+    })
+  }
+  getFechaHora(date:Date){
+    // let fecha=a.toLocaleDateString();
+    if(date.toString()!==''){
+
+     let hora=date.toLocaleTimeString();
+    // return fecha+' '+hora;
+    let dd = date.getDate();
+    let mm = date.getMonth() + 1; //January is 0!
+    let yyyy = date.getFullYear();
+    return yyyy+'-'+mm+'-'+dd+' '+hora
+    }
+    else{
+      return '';
+    }
   }
 }
