25bb063519a90ede5d04ef5d137499361ac9a1e7
daniel alvarez <daniel.alvarez@nextisolutions.com>
2021-11-19 09:15:11 -0500

    eedp before merge with develop

--- src/app/cred/citas/atencion-cred/plan/component/evaluacion-general/eedp/eedp.component.ts
+++ src/app/cred/citas/atencion-cred/plan/component/evaluacion-general/eedp/eedp.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EEDPComponent implements OnInit {
 
+  datosMeses: any[];
   constructor() { }
 
   ngOnInit(): void {
+    this.datosMeses = [
+      {
+        area: 'Coordinacion', mes_1: 1, mes_2: 1, mes_3: 1, mes_4: 1, mes_5: 1, mes_6: 1,
+        mes_7: 1, mes_8: 1, mes_9: 1, mes_10: 1, mes_12: 1, mes_15: 1, mes_18: 1, mes_21: 1, mes_24: 1,
+      },
+      {
+        area: 'Social', mes_1: 1, mes_2: 1, mes_3: 1, mes_4: 1, mes_5: 1, mes_6: 1,
+        mes_7: 1, mes_8: 1, mes_9: 1, mes_10: 1, mes_12: 1, mes_15: 1, mes_18: 1, mes_21: 1, mes_24: 1,
+      }
+    ]
   }
 
 }
