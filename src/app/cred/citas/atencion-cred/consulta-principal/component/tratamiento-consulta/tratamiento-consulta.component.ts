8a38de0a2137914983e58abdb115b0d9b428e2e2
christian <jc.yunguridata@gmail.com>
2021-11-24 16:36:13 -0500

    tratamiento-consulta

--- src/app/cred/citas/atencion-cred/consulta-principal/component/tratamiento-consulta/tratamiento-consulta.component.ts
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/tratamiento-consulta/tratamiento-consulta.component.ts
@@ -7,8 +7,37 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TratamientoConsultaComponent implements OnInit {
     data: any[] = [];
-
+    tratamiento: any[];
+    acuerdo: any[];
     constructor() {
+        this.tratamiento = [
+            {
+                tratamiento: "consulta 1"
+            },
+            {
+                tratamiento: "consulta 2"
+            },
+            {
+                tratamiento: "consulta 3"
+            },
+            {
+                tratamiento: "consulta 4"
+            },
+        ]
+        this.acuerdo = [
+            {
+                acuerdo: "acuerdo 1"
+            },
+            {
+                acuerdo: "acuerdo 2"
+            },
+            {
+                acuerdo: "acuerdo 3"
+            },
+            {
+                acuerdo: "acuerdo 4"
+            },
+        ]
     }
 
     ngOnInit(): void {
