a1762b6ec0afefc8068d1ad912a32bac9cb9d149
christian <jc.yunguridata@gmail.com>
2021-11-29 17:40:24 -0500

    evaluaciones-consulta

--- src/app/cred/citas/atencion-cred/consulta-principal/consulta-principal-routing.module.ts
+++ src/app/cred/citas/atencion-cred/consulta-principal/consulta-principal-routing.module.ts
@@ -1,26 +1,47 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { EvaluacionesConsultaComponent } from './component/evaluaciones-consulta/evaluaciones-consulta.component';
-import { PlanControlConsultaComponent } from './component/plan-control-consulta/plan-control-consulta.component';
-import { StepGeneralComponent } from './component/step-general/step-general.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { TestDesarrolloComponent } from '../plan/component/evaluacion-general/test-desarrollo/test-desarrollo.component';
+import { TestTepsiComponent } from '../plan/component/evaluacion-general/test-tepsi/test-tepsi.component';
+import {EvaluacionesConsultaComponent} from './component/evaluaciones-consulta/evaluaciones-consulta.component';
+import {PlanControlConsultaComponent} from './component/plan-control-consulta/plan-control-consulta.component';
+import {StepGeneralComponent} from './component/step-general/step-general.component';
+import {EEDPComponent} from "../plan/component/evaluacion-general/eedp/eedp.component";
+import {EvaluacionAlimentacionComponent} from "../plan/component/evaluacion-general/evaluacion-alimentacion/evaluacion-alimentacion.component";
 
 const routes: Routes = [
-  {
-    path: "consulta-principal",
-    component: StepGeneralComponent
-  },
-  {
-    path: "plan-control-consulta",
-    component: PlanControlConsultaComponent
-  },
-  {
-    path: "evaluaciones-consulta",
-    component: EvaluacionesConsultaComponent
-  }
+    {
+        path: "consulta-principal",
+        component: StepGeneralComponent
+    },
+    {
+        path: "plan-control-consulta",
+        component: PlanControlConsultaComponent
+    },
+    {
+        path: "evaluaciones-consulta",
+        component: EvaluacionesConsultaComponent
+    },
+    {
+        path: "test-tepsi-consulta",
+        component: TestTepsiComponent
+    },
+    {
+        path: "test-desarrollo-consulta",
+        component: TestDesarrolloComponent
+    },
+    {
+        path: "test-eedp-consulta",
+        component: EEDPComponent
+    },
+    {
+        path: "test-evaluacion-alimenticia-consulta",
+        component: EvaluacionAlimentacionComponent
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class ConsultaPrincipalRoutingModule { }
+export class ConsultaPrincipalRoutingModule {
+}
--- src/app/cred/citas/atencion-cred/plan/plan.module.ts
+++ src/app/cred/citas/atencion-cred/plan/plan.module.ts
@@ -1,56 +1,57 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { PlanRoutingModule } from './plan-routing.module';
-import { PrimeModule } from 'src/app/shared/prime/prime.module';
-import { CabeceraComponent } from './component/cabecera/cabecera.component';
-import { DatosGeneralesComponent } from './component/datos-generales/datos-generales.component';
-import { AntecendentesComponent } from './component/antecendentes/antecendentes.component';
-import { PlanAtencionIntegralComponent } from './component/plan-atencion-integral/plan-atencion-integral.component';
-import { PersonalComponent } from './component/antecendentes/personal/personal.component';
-import { FamiliarComponent } from './component/antecendentes/familiar/familiar.component';
-import { ViviendaComponent } from './component/antecendentes/vivienda/vivienda.component';
-import { EvaluacionGeneralModule } from './component/evaluacion-general/evaluacion-general.module';
+import {PlanRoutingModule} from './plan-routing.module';
+import {PrimeModule} from 'src/app/shared/prime/prime.module';
+import {CabeceraComponent} from './component/cabecera/cabecera.component';
+import {DatosGeneralesComponent} from './component/datos-generales/datos-generales.component';
+import {AntecendentesComponent} from './component/antecendentes/antecendentes.component';
+import {PlanAtencionIntegralComponent} from './component/plan-atencion-integral/plan-atencion-integral.component';
+import {PersonalComponent} from './component/antecendentes/personal/personal.component';
+import {FamiliarComponent} from './component/antecendentes/familiar/familiar.component';
+import {ViviendaComponent} from './component/antecendentes/vivienda/vivienda.component';
+import {EvaluacionGeneralModule} from './component/evaluacion-general/evaluacion-general.module';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InmunizacionesComponent } from './component/plan-atencion-integral/inmunizaciones/inmunizaciones.component';
-import { ControlCrecimientoComponent } from './component/plan-atencion-integral/control-crecimiento/control-crecimiento.component';
-import { DescartesComponent } from './component/plan-atencion-integral/descartes/descartes.component';
-import { SuplementacionesMicronutrientesComponent } from './component/plan-atencion-integral/suplementaciones-micronutrientes/suplementaciones-micronutrientes.component';
-import { TratamientoSeguimientoAnemiaComponent } from './component/plan-atencion-integral/tratamiento-seguimiento-anemia/tratamiento-seguimiento-anemia.component';
-import { SesionesAtencionTempranaComponent } from './component/plan-atencion-integral/sesiones-atencion-temprana/sesiones-atencion-temprana.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {InmunizacionesComponent} from './component/plan-atencion-integral/inmunizaciones/inmunizaciones.component';
+import {ControlCrecimientoComponent} from './component/plan-atencion-integral/control-crecimiento/control-crecimiento.component';
+import {DescartesComponent} from './component/plan-atencion-integral/descartes/descartes.component';
+import {SuplementacionesMicronutrientesComponent} from './component/plan-atencion-integral/suplementaciones-micronutrientes/suplementaciones-micronutrientes.component';
+import {TratamientoSeguimientoAnemiaComponent} from './component/plan-atencion-integral/tratamiento-seguimiento-anemia/tratamiento-seguimiento-anemia.component';
+import {SesionesAtencionTempranaComponent} from './component/plan-atencion-integral/sesiones-atencion-temprana/sesiones-atencion-temprana.component';
 
 @NgModule({
-  declarations: [
-    DatosGeneralesComponent,
-    CabeceraComponent,
-    AntecendentesComponent,
-    PlanAtencionIntegralComponent,
-    PersonalComponent,
-    FamiliarComponent,
-    ViviendaComponent,
-    InmunizacionesComponent,
-    ControlCrecimientoComponent,
-    DescartesComponent,
-    SuplementacionesMicronutrientesComponent,
-    TratamientoSeguimientoAnemiaComponent,
-    SesionesAtencionTempranaComponent,
-  ],
-  exports: [
-    InmunizacionesComponent,
-    ControlCrecimientoComponent,
-    SuplementacionesMicronutrientesComponent,
-    TratamientoSeguimientoAnemiaComponent,
-    SesionesAtencionTempranaComponent,
-    DescartesComponent
-  ],
-  imports: [
-    CommonModule,
-    EvaluacionGeneralModule,
-    PlanRoutingModule,
-    PrimeModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ]
+    declarations: [
+        DatosGeneralesComponent,
+        CabeceraComponent,
+        AntecendentesComponent,
+        PlanAtencionIntegralComponent,
+        PersonalComponent,
+        FamiliarComponent,
+        ViviendaComponent,
+        InmunizacionesComponent,
+        ControlCrecimientoComponent,
+        DescartesComponent,
+        SuplementacionesMicronutrientesComponent,
+        TratamientoSeguimientoAnemiaComponent,
+        SesionesAtencionTempranaComponent,
+    ],
+    exports: [
+        InmunizacionesComponent,
+        ControlCrecimientoComponent,
+        SuplementacionesMicronutrientesComponent,
+        TratamientoSeguimientoAnemiaComponent,
+        SesionesAtencionTempranaComponent,
+        DescartesComponent
+    ],
+    imports: [
+        CommonModule,
+        EvaluacionGeneralModule,
+        PlanRoutingModule,
+        PrimeModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ]
 })
-export class PlanModule { }
+export class PlanModule {
+}
