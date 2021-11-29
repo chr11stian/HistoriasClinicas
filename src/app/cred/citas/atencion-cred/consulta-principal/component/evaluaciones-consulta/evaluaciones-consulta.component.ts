4e8b7d9c0490048ac816817f8492bd935bf89131
christian <jc.yunguridata@gmail.com>
2021-11-29 16:48:47 -0500

    evaluacion

---
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/evaluaciones-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluaciones-consulta',
+  templateUrl: './evaluaciones-consulta.component.html',
+  styleUrls: ['./evaluaciones-consulta.component.css']
+})
+export class EvaluacionesConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
