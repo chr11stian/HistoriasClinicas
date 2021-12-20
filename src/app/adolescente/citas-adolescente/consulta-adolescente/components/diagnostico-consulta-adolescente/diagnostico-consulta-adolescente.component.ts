68a0f6652964b39c78550dd0ecbe54cfc29db43a
jonathan <jona.ml1011@gmail.com>
2021-12-20 16:23:22 -0500

    quitar un boton de consulta

--- src/app/adolescente/citas-adolescente/consulta-adolescente/components/diagnostico-consulta-adolescente/diagnostico-consulta-adolescente.component.ts
+++ src/app/adolescente/citas-adolescente/consulta-adolescente/components/diagnostico-consulta-adolescente/diagnostico-consulta-adolescente.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
+import { CieService } from 'src/app/obstetricia-general/services/cie.service';
+
 @Component({
   selector: 'app-diagnostico-consulta-adolescente',
   templateUrl: './diagnostico-consulta-adolescente.component.html',
@@ -8,25 +10,83 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class DiagnosticoConsultaAdolescenteComponent implements OnInit {
 
-  datosDiagnosticos: any;
+  listaDiagnosticos: any[] = [];
   form: FormGroup;
+  formDiagnostico: FormGroup;
+  dialogDiagnostic: boolean = false;
+  listaDeCIE: any;
 
   constructor(
     private fb: FormBuilder,
+    private CieService: CieService,
   ) {
     this.inicializarForm();
   }
 
   ngOnInit(): void {
+    this.form.patchValue({ diagnosticoText: 'gg' });
+    this.formDiagnostico.get("diagnosticoText").setValue("otro valor nuevo");
   }
 
-  inicializarForm(){
+  inicializarForm() {
     this.form = this.fb.group({
       habilidadesSociales: new FormControl(""),
       nutricional: new FormControl(""),
       recomendaciones: new FormControl(""),
-      
+
     });
+    this.formDiagnostico = this.fb.group({
+      autocompleDiag: new FormControl(""),
+      diagnosticoText: new FormControl(""),
+      diagnosticoCIE: new FormControl(""),
+    })
   }
 
+  openDialogDiagnostico() {
+    this.formDiagnostico.reset();
+    this.dialogDiagnostic = true;
+  }
+
+  filterCIE10(value) {
+    this.CieService.getCIEByDescripcion(value.query).subscribe((res: any) => {
+      this.listaDeCIE = res.object;
+    })
+  }
+
+  selectedOptionNameCIE(value) {
+    console.log('data selected ', value);
+    this.formDiagnostico.patchValue({ diagnosticoText: value.descripcionItem });
+    this.formDiagnostico.patchValue({ autocompleDiag: '' });
+    this.formDiagnostico.patchValue({ diagnosticoCIE: value });
+  }
+
+  aceptarNuevoDiagnostico() {
+    let diagnostico: diagnostico = {
+      diagnostico: this.formDiagnostico.value.diagnosticoText,
+      cie10: this.formDiagnostico.value.diagnosticoCIE
+    }
+    console.log('diagnostico ', diagnostico);
+    this.listaDiagnosticos.push(diagnostico);
+    this.dialogDiagnostic = false;
+  }
+
+  closeDialogDiagnostico() {
+    this.dialogDiagnostic = false;
+  }
+  selectedOption(event) {
+    this.formDiagnostico.patchValue({ diagnosticoText: event.descripcionItem });
+  }
+}
+
+export interface cie10 {
+  id: string,
+  codigoItem: string,
+  descripcionItem: string,
+  descripcionTipoItem: string,
+  tipoItem: string,
+}
+
+export interface diagnostico {
+  diagnostico: string,
+  cie10: cie10
 }
