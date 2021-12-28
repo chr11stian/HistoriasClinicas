7ceb6a30099a62e0e1ffece27feaed1e9489b34d
Jimmy <yimitopc@gmail.com>
2021-12-28 09:48:05 -0500

    ss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     production: false,
     bd: "hce",
     // baseUrl: "http://192.168.5.3:3012/api", //privada
-    baseUrl: "http://190.108.93.145:3012", //publica
+    baseUrl: "http://190.108.93.145:3012/api", //publica
 
     base_uri: "http://190.108.93.145:3012/api/hce/login/user",
 
