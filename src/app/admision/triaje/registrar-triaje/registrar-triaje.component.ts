e65162546f3c1c224736365ae1d179b2bcd39633
jonathan <jona.ml1011@gmail.com>
2022-01-19 14:08:15 -0500

    triaje

---
+++ src/app/admision/triaje/registrar-triaje/registrar-triaje.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-triaje',
+  templateUrl: './registrar-triaje.component.html',
+  styleUrls: ['./registrar-triaje.component.css']
+})
+export class RegistrarTriajeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
