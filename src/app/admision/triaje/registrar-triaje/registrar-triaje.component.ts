c4fed480609f78b126062de2809e1e7c8cf02764
jonathan <jona.ml1011@gmail.com>
2022-01-20 10:49:12 -0500

    servicios de triaje save

--- src/app/admision/triaje/registrar-triaje/registrar-triaje.component.ts
+++ src/app/admision/triaje/registrar-triaje/registrar-triaje.component.ts
@@ -4,6 +4,7 @@ import { MessageService } from 'primeng/api';
 import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ConsultasService } from 'src/app/obstetricia-general/gestante/atencion/consultorio-obstetrico/services/consultas.service';
 import { ConsultaObstetriciaService } from 'src/app/obstetricia-general/gestante/consulta/services/consulta-obstetricia/consulta-obstetricia.service';
+import { CuposTriajeService } from '../services/cupos-triaje/cupos-triaje.service';
 
 @Component({
   selector: 'app-registrar-triaje',
@@ -14,11 +15,12 @@ export class RegistrarTriajeComponent implements OnInit {
   formTriaje: FormGroup;
   triaje: Triaje;
   datosPersonales: any;
+  idCupo: string;
 
   constructor(
     private fb: FormBuilder,
     private dialog: DialogService,
-    private triajeService: ConsultaObstetriciaService,
+    private triajeService: CuposTriajeService,
     private config: DynamicDialogConfig,
     private ref: DynamicDialogRef,
     private messageService: MessageService,
@@ -26,6 +28,7 @@ export class RegistrarTriajeComponent implements OnInit {
     this.inicializarForm();
     console.log('data of listar ', config.data);
     this.datosPersonales = config.data;
+    this.idCupo = config.data.id;
   }
 
   ngOnInit(): void {
@@ -45,28 +48,24 @@ export class RegistrarTriajeComponent implements OnInit {
 
   recuperarDatos() {
     this.triaje = {
-      tipoDoc: 'DNI',
-      nroDoc: '72745818',
-      fecha: '2021-12-03 16:15:48',
-      funcionesVitales: {
-        t: parseFloat(this.formTriaje.value.temperatura),
-        presionSistolica: parseInt(this.formTriaje.value.presionSis),
-        presionDiastolica: parseInt(this.formTriaje.value.presionDias),
-        fc: parseInt(this.formTriaje.value.fc),
-        fr: parseInt(this.formTriaje.value.fr),
-        peso: parseFloat(this.formTriaje.value.peso),
-        talla: parseFloat(this.formTriaje.value.talla),
-      },
+      t: parseFloat(this.formTriaje.value.temperatura),
+      presionSistolica: parseInt(this.formTriaje.value.presionSis),
+      presionDiastolica: parseInt(this.formTriaje.value.presionDias),
+      fc: parseInt(this.formTriaje.value.fc),
+      fr: parseInt(this.formTriaje.value.fr),
+      peso: parseFloat(this.formTriaje.value.peso),
+      talla: parseFloat(this.formTriaje.value.talla),
+
     }
   }
 
   guardarTriaje() {
     this.recuperarDatos();
     console.log('data to show ', this.triaje);
-    // this.triajeService.postConsultaNoControl(this.triaje).subscribe((res: any) => {
-    //   console.log('se guardo ', res);
+    this.triajeService.postTriaje(this.idCupo, this.triaje).subscribe((res: any) => {
+      console.log('se guardo ', res);
 
-    // });
+    });
     this.messageService.add({ severity: 'success', summary: 'Exito', detail: 'Se guardo correctamente' });
     this.ref.close(this.triaje);
   }
@@ -85,16 +84,12 @@ export class RegistrarTriajeComponent implements OnInit {
   }
 }
 interface Triaje {
-  tipoDoc: string,
-  nroDoc: string,
-  fecha: string,
-  funcionesVitales: {
-    t: number,
-    presionSistolica: number,
-    presionDiastolica: number,
-    fc: number,
-    fr: number,
-    peso: number,
-    talla: number,
-  }
+  t: number,
+  presionSistolica: number,
+  presionDiastolica: number,
+  fc: number,
+  fr: number,
+  peso: number,
+  talla: number,
+
 }
