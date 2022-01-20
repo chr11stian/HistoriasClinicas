5a9ff086d4e9544a2e36140f920589e43bb27a5b
madaihq2020 <59493896+madaihq2020@users.noreply.github.com>
2022-01-20 10:19:41 -0500

    cupos paciente

--- src/app/admision/cupos/cupos.component.ts
+++ src/app/admision/cupos/cupos.component.ts
@@ -93,12 +93,9 @@ export class CuposComponent implements OnInit {
 
 
     }
-
     ngOnInit(): void {
         this.buildForm();
         this.formCuposOferta.get('SelectUPS').setValue(this.ServicoSelect);
-
-
         this.formCuposOferta.get('fechaBusqueda').setValue(this.datafecha);
         // this.getDataUPS();
         this.getListaUps();
@@ -176,29 +173,40 @@ export class CuposComponent implements OnInit {
             nroDoc: this.formCuposOferta.value.nroDoc,
         }
         this.pacienteService.getPacienteByNroDoc(auxNroDoc).subscribe((res: any) => {
-            this.dataPacientes = res.object
-            console.log('paciente por doc ', this.dataPacientes)
-            this.formCuposOferta.get('apePaterno').setValue(this.dataPacientes.apePaterno);
-            this.formCuposOferta.get('apeMaterno').setValue(this.dataPacientes.apeMaterno);
-            this.formCuposOferta.get('primerNombre').setValue(this.dataPacientes.primerNombre);
-            this.formCuposOferta.get('otrosNombres').setValue(this.dataPacientes.otrosNombres);
-            this.formCuposOferta.get('sexo').setValue(this.dataPacientes.sexo);
-            this.formCuposOferta.get('fechaNacimiento').setValue(this.obtenerFecha(this.dataPacientes.nacimiento.fechaNacimiento));
-            this.formCuposOferta.get('estadoCivil').setValue(this.dataPacientes.estadoCivil);
-            this.formCuposOferta.get('celular').setValue(this.dataPacientes.celular);
-            this.formCuposOferta.get('nacionalidad').setValue(this.dataPacientes.nacionalidad);
-            this.formCuposOferta.get('tipoSeguro').setValue(this.dataPacientes.tipoSeguro);
-            this.formCuposOferta.get('departamento').setValue(this.dataPacientes.domicilio.departamento);
-            this.formCuposOferta.get('provincia').setValue(this.dataPacientes.domicilio.provincia);
-            this.formCuposOferta.get('distrito').setValue(this.dataPacientes.domicilio.distrito);
-            this.formCuposOferta.get('centroPoblado').setValue(this.dataPacientes.domicilio.ccpp);
-            this.formCuposOferta.get('direccion').setValue(this.dataPacientes.domicilio.direccion);
-            this.formCuposOferta.get('tipoSeguro').setValue(this.dataPacientes.tipoSeguro);
-            if(this.dataPacientes.tipoSeguro=="SIS"){this.detallePago="GRATUITO"}
-            this.formCuposOferta.get('edadAnio').setValue(this.ageCalculatorA(this.obtenerFecha(this.dataPacientes.nacimiento.fechaNacimiento)));
-            this.formCuposOferta.get('edadMes').setValue(this.ageCalculatorM(this.dataPacientes.nacimiento.fechaNacimiento));
-            this.formCuposOferta.get('edadDia').setValue(this.ageCalculatorD(this.dataPacientes.nacimiento.fechaNacimiento));
-            // this.formCuposOferta.get('distrito').setValue(this.dataPacientes.domicilio.distrito);
+           if(res.object != null || res.object!= undefined){
+               this.dataPacientes = res.object
+               console.log('paciente por doc ', this.dataPacientes)
+               this.formCuposOferta.get('apePaterno').setValue(this.dataPacientes.apePaterno);
+               this.formCuposOferta.get('apeMaterno').setValue(this.dataPacientes.apeMaterno);
+               this.formCuposOferta.get('primerNombre').setValue(this.dataPacientes.primerNombre);
+               this.formCuposOferta.get('otrosNombres').setValue(this.dataPacientes.otrosNombres);
+               this.formCuposOferta.get('sexo').setValue(this.dataPacientes.sexo);
+               this.formCuposOferta.get('fechaNacimiento').setValue(this.obtenerFecha(this.dataPacientes.nacimiento.fechaNacimiento));
+               this.formCuposOferta.get('estadoCivil').setValue(this.dataPacientes.estadoCivil);
+               this.formCuposOferta.get('celular').setValue(this.dataPacientes.celular);
+               this.formCuposOferta.get('nacionalidad').setValue(this.dataPacientes.nacionalidad);
+               this.formCuposOferta.get('tipoSeguro').setValue(this.dataPacientes.tipoSeguro);
+               this.formCuposOferta.get('departamento').setValue(this.dataPacientes.domicilio.departamento);
+               this.formCuposOferta.get('provincia').setValue(this.dataPacientes.domicilio.provincia);
+               this.formCuposOferta.get('distrito').setValue(this.dataPacientes.domicilio.distrito);
+               this.formCuposOferta.get('centroPoblado').setValue(this.dataPacientes.domicilio.ccpp);
+               this.formCuposOferta.get('direccion').setValue(this.dataPacientes.domicilio.direccion);
+               this.formCuposOferta.get('tipoSeguro').setValue(this.dataPacientes.tipoSeguro);
+               if(this.dataPacientes.tipoSeguro=="SIS"){this.detallePago="GRATUITO"}
+               this.formCuposOferta.get('edadAnio').setValue(this.ageCalculatorA(this.obtenerFecha(this.dataPacientes.nacimiento.fechaNacimiento)));
+               this.formCuposOferta.get('edadMes').setValue(this.ageCalculatorM(this.dataPacientes.nacimiento.fechaNacimiento));
+               this.formCuposOferta.get('edadDia').setValue(this.ageCalculatorD(this.dataPacientes.nacimiento.fechaNacimiento));
+           }
+           else{
+               Swal.fire({
+                   icon: 'warning',
+                   title: 'Cupo',
+                   text: 'Paciente no encontrado en la Base de Datos, debe ingresar todo sus datos para crearle Historia Clínica',
+                   showConfirmButton: false,
+                   timer: 1500,
+               })
+               this.buscarNuevoPaciente();
+           }
         });
     }
 
