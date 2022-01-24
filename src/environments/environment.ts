fc5ea426ae5b1ede25e23ad643926f4a986b647b
Jimmy <yimitopc@gmail.com>
2022-01-24 12:15:13 -0500

    ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   bd: "hce",
-  // baseUrl: "http://192.168.5.3:3012/api", //privada
-  baseUrl: "http://190.108.93.145:3012/api", //publica
+  baseUrl: "http://192.168.5.3:3012/api", //privada
+  // baseUrl: "http://190.108.93.145:3012/api", //publica
 
   /**ACCESO AL LOGIN**/
-  // base_uri: "http://192.168.5.3:3010/login/hce",
-  base_uri: "http://190.108.93.145:3010/login/hce",
+  base_uri: "http://192.168.5.3:3010/login/hce",
+  // base_uri: "http://190.108.93.145:3010/login/hce",
   // base_uri: "http://190.108.93.145:3012/api/hce/login/user",
 };
 
