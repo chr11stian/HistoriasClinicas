4e26b637b87db831cbb14267554b6cdda5a6d14d
Leticia Olazabal <giulianaolazabal7@gmail.com>
2022-01-24 16:39:34 -0500

    Agregando ambientes, turnos y roles dependiendo a cada ipress - usuario ipress

---
+++ src/app/admision/ipress-ambientes/ipress-ambientes.component.ts
@@ -0,0 +1,154 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IpressService } from 'src/app/core/services/ipress/ipress.service';
+import { UpsService } from 'src/app/mantenimientos/services/ups/ups.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-ipress-ambientes',
+  templateUrl: './ipress-ambientes.component.html',
+  styleUrls: ['./ipress-ambientes.component.css']
+})
+export class IpressAmbientesComponent implements OnInit {
+
+  formAmbiente: FormGroup;
+  isUpdateAmbiente: boolean = false;
+  UPSList: any[];
+  ambientes: any[];
+  ambienteDialog: boolean;
+  idIpress: string;
+
+  constructor(
+    private ipressservice: IpressService,
+    private upsService: UpsService,
+    private formBuilder: FormBuilder
+  ) {
+    this.idIpress = "616de45e0273042236434b51";
+    this.buildForm();
+    this.getIpressId();
+    this.getUPS();
+  }
+
+  getIpressId() {
+    this.ipressservice.getIpressID(this.idIpress).subscribe((res: any) => {
+      this.ambientes = res.object.ambientes;
+    });
+  }
+  getUPS() {
+    this.upsService.getUPS().subscribe((res: any) => {
+      this.UPSList = res.object;
+    })
+  }
+  buildForm() {
+    this.formAmbiente = this.formBuilder.group({
+      codAmbiente: ['', [Validators.required]],
+      ambiente: ['', [Validators.required]],
+      nombreUPS: ['', [Validators.required]],
+    })
+  }
+  openNew() {
+    this.isUpdateAmbiente = false;
+    this.formAmbiente.reset();
+    this.formAmbiente.get('codAmbiente').setValue("");
+    this.formAmbiente.get('ambiente').setValue("");
+    this.formAmbiente.get('nombreUPS').setValue("");
+    this.ambienteDialog = true;
+  }
+  editarAmbiente(rowData) {
+    this.isUpdateAmbiente = true;
+    console.log(rowData);
+    this.formAmbiente.get('codAmbiente').setValue(rowData.codAmbiente);
+    this.formAmbiente.get('ambiente').setValue(rowData.ambiente);
+    this.formAmbiente.get('nombreUPS').setValue(rowData.nombreUPS);
+    this.ambienteDialog = true;
+  }
+  saveEdicionAmbiente() {
+    const req = {
+      codAmbiente: this.formAmbiente.value.codAmbiente,
+      ambiente: this.formAmbiente.value.ambiente,
+      nombreUPS: this.formAmbiente.value.nombreUPS
+    }
+    this.ipressservice.editAmbienteIpress(this.idIpress, req).subscribe(
+      result => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Editado correctamente',
+          text: '',
+          showConfirmButton: false,
+          timer: 1500,
+        })
+        this.getIpressId();
+        this.formAmbiente.reset();
+        this.ambienteDialog=false;
+      }
+    )
+  }
+
+  tituloAmbiente() {
+    if (this.isUpdateAmbiente) return "Edite Ambiente";
+    else return "Ingrese Nuevo Ambiente";
+  }
+  saveAmbiente(rowData) {
+    const req = {
+      codAmbiente: this.formAmbiente.value.codAmbiente,
+      ambiente: this.formAmbiente.value.ambiente,
+      nombreUPS: this.formAmbiente.value.nombreUPS
+    }
+
+    this.ipressservice.createAmbienteIpress(this.idIpress, req).subscribe(
+      result => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Agregado correctamente',
+          text: '',
+          showConfirmButton: false,
+          timer: 1500,
+        })
+        this.getIpressId();
+        this.formAmbiente.reset();
+        this.ambienteDialog = false;
+      }
+    )
+  }
+  
+  eliminarAmbiente(rowData) {
+    this.isUpdateAmbiente = false;
+    Swal.fire({
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      icon: 'warning',
+      title: 'Estas seguro de eliminar',
+      text: '',
+      showConfirmButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.ipressservice.deleteAmbienteIpress(this.idIpress, rowData.codAmbiente).subscribe(
+          result => {
+            this.getIpressId();
+          }
+        );
+        Swal.fire({
+          icon: 'success',
+          title: 'Eliminado correctamente',
+          text: '',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    })
+  }
+  canceled() {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Cancelado...',
+      text: '',
+      showConfirmButton: false,
+      timer: 1000
+    })
+    this.ambienteDialog = false;
+    this.formAmbiente.reset();
+  }
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/admision/ipress-turnos/ipress-turnos.component.ts
@@ -0,0 +1,175 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IpressService } from 'src/app/core/services/ipress/ipress.service';
+import { TipoTurnoService } from 'src/app/mantenimientos/services/tipo-turno.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-ipress-turnos',
+  templateUrl: './ipress-turnos.component.html',
+  styleUrls: ['./ipress-turnos.component.css']
+})
+export class IpressTurnosComponent implements OnInit {
+  formTurno: FormGroup;
+  isUpdateTurno: boolean = false;
+  idIpress: string = "";
+  tipoTurnosList: any[];
+  turnos: any[];
+  turnoDialog: boolean;
+
+  constructor(
+    private ipressservice: IpressService,
+    private tipoturnoService: TipoTurnoService,
+    private formBuilder: FormBuilder
+  ) { 
+    this.idIpress = "616de45e0273042236434b51";
+    this.buildForm();
+    this.getIpressId();
+    this.getTiposTurno();
+  }
+  getIpressId() {
+    this.ipressservice.getIpressID(this.idIpress).subscribe((res: any) => {
+      this.turnos = res.object.turnos;
+    });
+  }
+  getTiposTurno() {
+    this.tipoturnoService.getTipoTurnos().subscribe((res: any) => {
+      this.tipoTurnosList = res.object;
+    })
+  }
+  buildForm() {
+    this.formTurno = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      nroHoras: ['', [Validators.required]],
+      horaInicio: ['', [Validators.required]],
+      horaFin: ['', [Validators.required]],
+    })
+  }
+  openNew() {
+    this.isUpdateTurno = false;
+    this.formTurno.reset();
+    this.formTurno.get('nombre').setValue("");
+    this.formTurno.get('nroHoras').setValue("");
+    this.formTurno.get('horaInicio').setValue("");
+    this.formTurno.get('horaFin').setValue("");
+    this.turnoDialog = true;
+  }
+
+  editarTurno(rowData) {
+    this.isUpdateTurno = true;
+    this.formTurno.get('nombre').setValue(this.tipoTurnosList.find(turno => turno.nombre === rowData.nombre));
+    this.formTurno.get('nroHoras').setValue(rowData.nroHoras);
+    this.formTurno.get('horaInicio').setValue(new Date(`2021-01-01 ${rowData.horaInicio}`));
+    this.formTurno.get('horaFin').setValue(new Date(`2021-01-01 ${rowData.horaFin}`));
+    this.turnoDialog = true;
+  }
+  selectedTipoTurno() {
+    this.formTurno.get('nroHoras').setValue(this.formTurno.value.nombre.nroHoras);
+  }
+  selectedHoraInicio() {
+    let horaFin = new Date(this.formTurno.value.horaInicio);
+    let nroHoras = this.formTurno.value.nroHoras;
+    horaFin.setHours(horaFin.getHours() + nroHoras);
+    this.formTurno.get('horaFin').setValue(new Date(`2021-01-01 ${horaFin.getHours()}: ${horaFin.getMinutes()}:00`));
+  }
+  saveTurno(rowData) {
+    let horaInicio = new Date(this.formTurno.value.horaInicio);
+    let horaFin = new Date(this.formTurno.value.horaFin);
+    const req = {
+      nombre: this.formTurno.value.nombre.nombre,
+      abreviatura: this.formTurno.value.nombre.abreviatura,
+      nroHoras: this.formTurno.value.nroHoras,
+      horaInicio: `${horaInicio.getHours()}:${horaInicio.getMinutes()}:00`,
+      horaFin: `${horaFin.getHours()}:${horaFin.getMinutes()}:00`
+    }
+
+    this.ipressservice.createTurnoIpress(this.idIpress, req).subscribe(
+      result => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Agregado correctamente',
+          text: '',
+          showConfirmButton: false,
+          timer: 1500,
+        })
+        this.getIpressId();
+        this.formTurno.reset();
+        this.formTurno.get('nombre').setValue("");
+        this.turnoDialog = false;
+      }
+    )
+  }
+
+  tituloTurno() {
+    if (this.isUpdateTurno) return "Edite Turno";
+    else return "Ingrese Nuevo Turno";
+  }
+  saveEdicionTurno() {
+    let horaInicio = new Date(this.formTurno.value.horaInicio);
+    let horaFin = new Date(this.formTurno.value.horaFin);
+    const req = {
+      nombre: this.formTurno.value.nombre.nombre,
+      abreviatura: this.formTurno.value.nombre.abreviatura,
+      nroHoras: this.formTurno.value.nroHoras,
+      horaInicio: `${horaInicio.getHours()}:${horaInicio.getMinutes()}:00`,
+      horaFin: `${horaFin.getHours()}:${horaFin.getMinutes()}:00`
+    }
+    this.ipressservice.editTurnoIpress(this.idIpress, req).subscribe(
+      result => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Editado correctamente',
+          text: '',
+          showConfirmButton: false,
+          timer: 1500,
+        })
+        this.getIpressId();
+        this.formTurno.reset();
+        this.formTurno.get('nombre').setValue("");
+        this.turnoDialog = false;
+      }
+    )
+  }
+  eliminarTurno(rowData) {
+    this.isUpdateTurno = false;
+    Swal.fire({
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      icon: 'warning',
+      title: 'Estas seguro de eliminar',
+      text: '',
+      showConfirmButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.ipressservice.deleteTurnoIpress(this.idIpress, rowData.abreviatura).subscribe(
+          result => {
+            this.getIpressId();
+          }
+        );
+        Swal.fire({
+          icon: 'success',
+          title: 'Eliminado correctamente',
+          text: '',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    })
+  }
+  
+  canceled() {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Cancelado...',
+      text: '',
+      showConfirmButton: false,
+      timer: 1000
+    })
+    this.turnoDialog = false;
+    this.formTurno.reset();
+    this.formTurno.get('nombre').setValue("");
+  }
+  ngOnInit(): void {
+  }
+
+}
