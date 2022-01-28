394b48f2075d2ee62b030182333f55433efdd2e3
jonathan <jona.ml1011@gmail.com>
2022-01-28 12:38:23 -0500

    steps de consulta prinicipal cred

---
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/examenes-auxiliares-consulta/examenes-auxiliares-consulta.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examenes-auxiliares-consulta',
+  templateUrl: './examenes-auxiliares-consulta.component.html',
+  styleUrls: ['./examenes-auxiliares-consulta.component.css']
+})
+export class ExamenesAuxiliaresConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  save(){
+    
+  }
+
+}
