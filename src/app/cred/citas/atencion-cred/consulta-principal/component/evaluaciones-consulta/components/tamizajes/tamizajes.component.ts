92315cbd0f9e06a1a19a2f0ed18b722a625bf443
madaihq2020 <59493896+madaihq2020@users.noreply.github.com>
2022-02-08 09:54:32 -0500

    tamizaje auditivo-cred

--- src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/components/tamizajes/tamizajes.component.ts
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/components/tamizajes/tamizajes.component.ts
@@ -8,6 +8,10 @@ import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 })
 export class TamizajesComponent implements OnInit {
   formTamizajeCred:FormGroup;
+  sino = [
+    { label: 'SI', value: true },
+    { label: 'NO', value: false }
+  ];
   constructor(private formBuilder: FormBuilder) {
     this.builForm();
   }
@@ -17,6 +21,13 @@ export class TamizajesComponent implements OnInit {
   builForm(){
     this.formTamizajeCred = this.formBuilder.group({
       tamizajeAuditivo:new FormControl(''),
+      prematuro:new FormControl(''),
+      uci:new FormControl(''),
+      billirubina:new FormControl(''),
+      perdidaAudicion:new FormControl(''),
+      infeccionOido:new FormControl(''),
+      meningitis:new FormControl(''),
+      expuestoSonido:new FormControl(''),
       ojoDerecho:new FormControl(''),
       ojoIzquierdo:new FormControl(''),
       descripcionTamizajeOcular:new FormControl(''),
