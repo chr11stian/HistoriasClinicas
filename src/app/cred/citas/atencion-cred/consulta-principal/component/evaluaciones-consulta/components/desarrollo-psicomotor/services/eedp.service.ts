f3dc07450b7057a8277e6d4ae9fc4749ea8134c0
jonathan <jona.ml1011@gmail.com>
2022-02-16 12:24:13 -0500

    service para eedp

---
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/components/desarrollo-psicomotor/services/eedp.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EedpService {
+  base_url = environment.baseUrl;
+  bd = environment.bd;
+
+  constructor(private http: HttpClient) { }
+
+  postAgregarEEDP(idConsulta:string, dataEEDP) {
+    return this.http.post(`${this.base_url}/${this.bd}/cred/consulta/evaluacion/eedp/${idConsulta}`, dataEEDP);
+  }
+  getEEDPxIdConsulta(idConsulta:string) {
+    return this.http.get(`${this.base_url}/${this.bd}/cred/consulta/evaluacion/eedp/${idConsulta}`);
+  }
+  getListarEEDPxHistoria(nroHcl:string) {
+    return this.http.get(`${this.base_url}/${this.bd}/cred/planintegral/evaluacion/eedp/${nroHcl}`)
+  }
+  putEEDPxByIdConsulta(idConsulta:string, dataEEDP) {
+    return this.http.put(`${this.base_url}/${this.bd}/cred/consulta/evaluacion/eedp/${idConsulta}`, dataEEDP);
+  }
+}
