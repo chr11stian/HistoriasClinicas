d7e138e35ffbd3a6b9aedf86c4ca522341fa54b3
Jimmy <yimitopc@gmail.com>
2022-02-23 16:30:21 -0500

    obs

--- src/app/obstetricia-general/gestante/atencion/h-clinica-materno-perinatal/component/datos-generales-filiacion/datos-generales-filiacion.component.ts
+++ src/app/obstetricia-general/gestante/atencion/h-clinica-materno-perinatal/component/datos-generales-filiacion/datos-generales-filiacion.component.ts
@@ -1,12 +1,10 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {FiliancionService} from "../../services/filiancion-atenciones/filiancion.service";
-import {formatDate} from "@angular/common";
 import Swal from "sweetalert2";
 import {ObstetriciaGeneralService} from "../../../../../services/obstetricia-general.service";
-import {validate} from "codelyzer/walkerFactory/walkerFn";
-import {PersonalService} from "../../../../../../core/services/personal-services/personal.service";
 import {image} from "../../../../../../../assets/images/image.const";
+import {UbicacionService} from "../../../../../../mantenimientos/services/ubicacion/ubicacion.service";
 
 @Component({
     selector: 'app-datos-generales-filiacion',
@@ -20,14 +18,12 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
     provincias: any;
     distritos: any
     options: any;
-    stadoCivil: any;
     familiares: any
     studies: any;
     dataPacientes: any;
     id: any;
     fechanacimiento: string;
     edad: any;
-    // idDocumento: string;
     dataIDfiliacion: any;
     dataPacientesReniec: any;
     imagePath: string = image;
@@ -39,9 +35,17 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
     nroDocRecuperado: string;
 
     fechaConvertido: string;
+    dataDepartamentos: any;
+    dataProvincia: any;
+    ProvinciaIDSelct: any;
+    dataDistrito: any;
+    DistritoIDSelct: any;
+    dataCentroPoblado: any;
+    DepartamentoIDSelct: any;
 
-    constructor(private formDatosGenerales: FormBuilder,
+    constructor(private fb: FormBuilder,
                 private filiancionService: FiliancionService,
+                private ubicacionService: UbicacionService,
                 private obstetriciaGeneralService: ObstetriciaGeneralService) {
 
         this.tipoDocRecuperado = this.obstetriciaGeneralService.tipoDoc;
@@ -55,45 +59,13 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
         ];
 
         this.studies = [
-            {gradoInstruccion: 'Analfabeta', code: 'Analfabeta'},
-            {gradoInstruccion: 'Primaria', code: 'Primaria'},
-            {gradoInstruccion: 'Secundaria', code: 'Secundaria'},
-            {gradoInstruccion: 'Superior', code: 'Superior'},
-            {gradoInstruccion: 'Superior No Univ.', code: 'SuperiorNo'},
+            'ANALFABETO',
+            'PRIMARIA INCOMPLETA',
+            'PRIMARIA COMPLETA',
+            'SECUNDARIA INCOMPLETA',
+            'SECUNDARIA COMPLETA',
+            'SUPERIOR'
         ];
-
-        this.stadoCivil = [
-            {estadocivil: 'Casado', code: 'Casado'},
-            {estadocivil: 'Combiviente', code: 'Combiviente'},
-            {estadocivil: 'Soltero', code: 'Soltero'},
-        ];
-
-        this.departamentos = [
-            {departamento: 'Lima'},
-            {departamento: 'Arequipa'},
-            {departamento: 'Puno'},
-            {departamento: 'Madre de Dios'},
-            {departamento: 'Cusco'},
-            {departamento: 'Loreto'},
-            {departamento: 'Cajamarca'},
-            {departamento: 'Ayacucho'},
-        ];
-        this.provincias = [
-            {provincia: 'Cusco'},
-            {provincia: 'Lima'},
-            {provincia: 'Arequipa'},
-            {provincia: 'Puno'},
-            {provincia: 'Madre de Dios'},
-            {provincia: 'Loreto'},
-            {provincia: 'Cajamarca'},
-            {provincia: 'Ayacucho'},
-        ];
-        this.distritos = [
-            {distrito: 'Cusco'},
-            {distrito: 'Santiago'},
-            {distrito: 'San sebastian'},
-        ];
-
     }
 
 
@@ -101,6 +73,7 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
         console.log("IdRecuperado", this.idRecuperado);
         console.log("TipoDocRecuperado", this.tipoDocRecuperado);
         console.log("NroDocRecuparado", this.nroDocRecuperado);
+        this.getDepartamentos();
         this.obternerFechaActual();
         this.buildForm();
 
@@ -110,6 +83,168 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
 
     }
 
+    buildForm() {
+        this.formDatos_Generales = this.fb.group({
+            HCL: new FormControl(''),
+            apePaterno: new FormControl(''),
+            apeMaterno: new FormControl(''),
+            primerNombre: new FormControl(''),
+            establecimiento: new FormControl(''),
+            estabOrigen: new FormControl(''),
+            aplica: new FormControl(''),
+            referencia: new FormControl(''),
+            codAficiaconSIS: new FormControl(''),
+            tipoSeguro: new FormControl(''),
+            docIndentidad: new FormControl(''),
+            fechaNacimiento: new FormControl(''),
+            ocupacion: new FormControl(''),
+            edad: new FormControl(''),
+            direccion: new FormControl(''),
+            gradoInstruccion: new FormControl(''),
+            amiosAprobados: new FormControl(''),
+            nombreNroSector: new FormControl(''),
+            estadoCivil: new FormControl(''),
+            departamento: new FormControl(''),
+            provincia: new FormControl(''),
+            distrito: new FormControl(''),
+            nombreRN: new FormControl(''),
+            pabreRN: new FormControl(''),
+            religion: new FormControl(''),
+            cel1: new FormControl('', [Validators.required, Validators.min(1), Validators.max(9)]),
+            cel2: new FormControl(''),
+            idioma: new FormControl(''),
+        })
+    }
+
+    /**Lista todos los departamento **/
+    getDepartamentos() {
+        this.ubicacionService.getDepartamentos().subscribe((resp: any) => {
+            this.dataDepartamentos = resp.object;
+            console.log("Departamento", this.dataDepartamentos);
+        });
+    }
+
+    /**Selecciona un departamento y lista las provincias**/
+    selectedDepartamento() {
+        let depa = this.formDatos_Generales.value.departamento;
+        this.dataDepartamentos.forEach(object => {
+            if (object.departamento === depa) {
+                console.log("Departamento:", object);
+                this.DepartamentoIDSelct = object.iddd
+            }
+        });
+        let dpto = {
+            iddd: this.DepartamentoIDSelct
+        }
+        this.dataDistrito = '';
+        this.dataCentroPoblado = '';
+        this.dataProvincia = '';
+        this.ubicacionService.getProvincias(dpto).subscribe((res: any) => {
+            this.dataProvincia = res.object;
+        });
+    }
+
+    /**Selecciona un Provincia y lista los Distritos**/
+    selectedProvincia() {
+        // DistritoIDSelct
+        let provinciaX = this.formDatos_Generales.value.provincia;
+        this.dataProvincia.forEach(object => {
+            if (object.provincia === provinciaX) {
+                console.log("Provincia:", object);
+                this.ProvinciaIDSelct = object.idpp
+            }
+        });
+
+        let provincia = {
+            iddd: this.DepartamentoIDSelct,
+            idpp: this.ProvinciaIDSelct,
+        };
+        this.dataCentroPoblado = '';
+        this.ubicacionService.getDistritos(provincia).subscribe((res: any) => {
+            this.dataDistrito = res.object;
+        });
+    }
+
+    /**Selecciona un Distrito y lista los Centros Poblados**/
+    selectedDistrito() {
+        let distritoX = this.formDatos_Generales.value.distrito;
+        this.dataDistrito.forEach(object => {
+            if (object.distrito === distritoX) {
+                console.log("Distrito:", object);
+                this.DistritoIDSelct = object.iddis
+            }
+        });
+        let distrito = {
+            iddd: this.DepartamentoIDSelct,
+            idpp: this.ProvinciaIDSelct,
+            iddis: this.DistritoIDSelct,
+        }
+        this.ubicacionService.getCentroPoblado(distrito).subscribe((res: any) => {
+            this.dataCentroPoblado = res.object;
+        });
+    }
+
+    listarUbicacionPacienteProvincias() {
+        let Departamento = this.dataPacientes.domicilio.departamento;
+        console.log("Departamento XXX", this.dataDepartamentos)
+
+        console.log("Departamento:", Departamento);
+        this.dataDepartamentos.forEach(object => {
+            if (object.departamento === Departamento) {
+                console.log("Departamento:", object);
+                this.DepartamentoIDSelct = object.iddd
+            }
+        });
+        let dpto = {
+            iddd: this.DepartamentoIDSelct
+        }
+        this.ubicacionService.getProvincias(dpto).subscribe((res: any) => {
+            this.dataProvincia = res.object;
+            console.log("PROVINCIA:", this.dataProvincia);
+            this.listarUbicacionPacientedistritos();
+        });
+    }
+
+    listarUbicacionPacientedistritos() {
+
+        let Provincia = this.dataPacientes.domicilio.provincia;
+
+        this.dataProvincia.forEach(object => {
+            if (object.provincia === Provincia) {
+                console.log("Provincia:", object);
+                this.ProvinciaIDSelct = object.idpp
+            }
+        });
+        let provincia1 = {
+            iddd: this.DepartamentoIDSelct,
+            idpp: this.ProvinciaIDSelct,
+        };
+        this.ubicacionService.getDistritos(provincia1).subscribe((res: any) => {
+            this.dataDistrito = res.object;
+            this.listarUbicacionPacienteCCPP();
+        });
+
+    }
+
+    listarUbicacionPacienteCCPP() {
+
+        let Distrito = this.dataPacientes.domicilio.distrito;
+
+        this.dataDistrito.forEach(object => {
+            if (object.distrito === Distrito) {
+                console.log("Distrito:", object);
+                this.DistritoIDSelct = object.iddis
+            }
+        });
+        let distrito1 = {
+            iddd: this.DepartamentoIDSelct,
+            idpp: this.ProvinciaIDSelct,
+            iddis: this.DistritoIDSelct,
+        }
+        this.ubicacionService.getCentroPoblado(distrito1).subscribe((res: any) => {
+            this.dataCentroPoblado = res.object;
+        });
+    }
 
     obternerFechaActual() {
         this.fecha = new Date();
@@ -203,6 +338,7 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
         this.filiancionService.getPacienteNroDocFiliacion(this.tipoDocRecuperado, this.nroDocRecuperado).subscribe((res: any) => {
             this.dataPacientes = res.object;
             this.traerDataReniec();
+            this.listarUbicacionPacienteProvincias();
             console.log('paciente por doc ', this.dataPacientes)
             this.formDatos_Generales.get('apePaterno').setValue(this.dataPacientes.apePaterno);
             this.formDatos_Generales.get('apeMaterno').setValue(this.dataPacientes.apeMaterno);
@@ -211,12 +347,13 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
             this.formDatos_Generales.get('docIndentidad').setValue(this.dataPacientes.nroDoc);
             this.formDatos_Generales.get('establecimiento').setValue(this.dataPacientes.nombreEESS);
             this.formDatos_Generales.get('estadoCivil').setValue(this.dataPacientes.estadoCivil);
-
-            this.formDatos_Generales.get('codAficiaconSIS').setValue(this.dataPacientes.codAficiaconSIS);
+            this.formDatos_Generales.get('codAficiaconSIS').setValue(this.dataPacientes.codSeguro);
+            this.formDatos_Generales.get('tipoSeguro').setValue(this.dataPacientes.tipoSeguro);
             this.formDatos_Generales.get('direccion').setValue(this.dataPacientes.domicilio.direccion);
             this.formDatos_Generales.get('departamento').setValue(this.dataPacientes.domicilio.departamento);
             this.formDatos_Generales.get('provincia').setValue(this.dataPacientes.domicilio.provincia);
             this.formDatos_Generales.get('distrito').setValue(this.dataPacientes.domicilio.distrito);
+            this.formDatos_Generales.get('nombreNroSector').setValue(this.dataPacientes.domicilio.ccpp);
             this.formDatos_Generales.get('gradoInstruccion').setValue(this.dataPacientes.gradoInstruccion);
             this.formDatos_Generales.get('cel1').setValue(this.dataPacientes.celular);
 
@@ -275,39 +412,6 @@ export class DatosGeneralesFiliacionComponent implements OnInit {
             this.formDatos_Generales.get('cel2').setValue(this.dataIDfiliacion.nroCelular[1]);
             this.formDatos_Generales.get('edad').setValue(this.dataIDfiliacion.edad);
         });
-    }
-
-
-    buildForm() {
-        this.formDatos_Generales = this.formDatosGenerales.group({
-            HCL: new FormControl(''),
-            apePaterno: new FormControl(''),
-            apeMaterno: new FormControl(''),
-            primerNombre: new FormControl(''),
-            establecimiento: new FormControl(''),
-            estabOrigen: new FormControl(''),
-            aplica: new FormControl(''),
-            referencia: new FormControl(''),
-            codAficiaconSIS: new FormControl(''),
-            docIndentidad: new FormControl(''),
-            fechaNacimiento: new FormControl(''),
-            ocupacion: new FormControl(''),
-            edad: new FormControl(''),
-            direccion: new FormControl(''),
-            gradoInstruccion: new FormControl(''),
-            amiosAprobados: new FormControl(''),
-            nombreNroSector: new FormControl(''),
-            estadoCivil: new FormControl(''),
-            departamento: new FormControl(''),
-            provincia: new FormControl(''),
-            distrito: new FormControl(''),
-            nombreRN: new FormControl(''),
-            pabreRN: new FormControl(''),
-            religion: new FormControl(''),
-            cel1: new FormControl('', [Validators.required, Validators.min(1), Validators.max(9)]),
-            cel2: new FormControl(''),
-            idioma: new FormControl(''),
-        })
     }
 
 
