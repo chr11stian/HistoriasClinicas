0ab6814869d0363409b601a6eeb88020b9a10ba1
Leticia Olazabal <giulianaolazabal7@gmail.com>
2022-02-25 15:45:12 -0500

    creando componente en caja

--- src/app/caja/caja.module.ts
+++ src/app/caja/caja.module.ts
@@ -1,17 +1,19 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {PagosComponent} from './pagos/pagos.component';
 import {MantenimentosRoutingModule} from "../mantenimientos/mantenimentos-routing.module";
 import {CajaRoutingModule} from "./historia-routing.module";
 import { TipoComprobanteComponent } from './tipo-comprobante/tipo-comprobante.component';
 import {ToolbarModule} from "primeng/toolbar";
 import {PrimeModule} from "../shared/prime/prime.module";
+import { PagoProcedimientosComponent } from './pago-procedimientos/pago-procedimientos.component';
+import { PagosComponent } from './pagos/pagos.component';
 
 
 @NgModule({
     declarations: [
         PagosComponent,
-        TipoComprobanteComponent
+        TipoComprobanteComponent,
+        PagoProcedimientosComponent
     ],
     exports: [PagosComponent],
     imports: [
---
+++ src/app/caja/pago-procedimientos/pago-procedimientos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pago-procedimientos',
+  templateUrl: './pago-procedimientos.component.html',
+  styleUrls: ['./pago-procedimientos.component.css']
+})
+export class PagoProcedimientosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
