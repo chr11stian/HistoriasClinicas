b9f46f24de58f36142d0b350e58c759b2f33a821
jonathan <jona.ml1011@gmail.com>
2022-03-01 10:07:37 -0500

    git estilos de eedpp

--- src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/components/desarrollo-psicomotor/services/eedp.service.ts
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/components/desarrollo-psicomotor/services/eedp.service.ts
@@ -37,4 +37,11 @@ export class EedpService {
       .then(data => { return data; })
       .catch(error => { return error.error });
   }
+  getPromiseEEDPxIdConsulta(idConsulta: string) {
+    return this.http.get<any>(`${this.base_url}/${this.bd}/cred/consulta/evaluacion/eedp/${idConsulta}`)
+      .toPromise()
+      .then(res => <any>res.object)
+      .then(data => { return data; })
+      .catch(error => { return error.error });
+  }
 }
