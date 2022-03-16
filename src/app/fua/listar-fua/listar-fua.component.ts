95ae9d71a70d24a3050eaf6341bbc793298e133a
Jonathan <jona.ml1011@gmail.com>
2022-03-16 10:01:41 -0500

    crear listar fua

---
+++ src/app/fua/listar-fua/listar-fua.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-fua',
+  templateUrl: './listar-fua.component.html',
+  styleUrls: ['./listar-fua.component.css']
+})
+export class ListarFuaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
