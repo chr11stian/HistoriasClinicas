657623703b0ed01db81359281a62de210696a3f3
madaihq2020 <59493896+madaihq2020@users.noreply.github.com>
2022-03-28 10:13:08 -0500

    cambios

---
+++ src/app/his/services/services.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+  urlServer = environment.baseUrl
+  bd = environment.bd
+
+  constructor(private http: HttpClient) { }
+  getListaUpsAux(idConsulta: string) {
+    return this.http.get(`${this.urlServer}/${this.bd}/his/generar/lista/upsAux/his/${idConsulta}`);
+  }
+  generarHisPorUpsAux(upsAux: string,idConsulta:string) {
+    const params =new HttpParams({
+      fromObject:{
+        upsAux:upsAux
+      }
+    })
+    return this.http.get(`${this.urlServer}/${this.bd}/his/generar/${idConsulta}`,{params});
+  }
+  saveHis(data:any){
+    return this.http.post(`${this.urlServer}/${this.bd}/his/guardar/`, data)
+  }
+  updateHis(idHis:string,data:any){
+    return this.http.post(`${this.urlServer}/${this.bd}/his/actualizar/${idHis}/`, data)
+  }
+  getListHisForUpsAux(upsAux:string,data){
+    const params =new HttpParams({
+      fromObject:{
+        upsAux:upsAux
+      }
+    })
+    return this.http.get(`${this.urlServer}/${this.bd}/his/listar/por/ups/`,{params});
+  }
+
+
+}
