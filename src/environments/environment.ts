765d574d7c88925b4bae04af613387f51b1a8f71
Juan Christian Yunguri Ttito <jc.yunguri@gmail.com>
2022-03-30 14:46:33 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,8 +18,8 @@ export const environment = {
     // base_url_pide: "http://190.108.93.145:3011/pide/datos-sis",
 
     /***ACCESO A REPORTE TRATAMIENTOS****/
-    base_urlTx:"http://192.168.5.3:8200",
-    // base_urlTx: "http://190.108.93.145:8200",
+    //base_urlTx:"http://192.168.5.3:8200",
+    base_urlTx: "http://190.108.93.145:8200",
 
 };
 
