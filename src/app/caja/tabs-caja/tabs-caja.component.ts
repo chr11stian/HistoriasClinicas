9e980472374f31ac20b84fd53276eba1b840a029
Giuliana Olazabal <giulianaolazabal7@gmail.com>
2022-04-16 15:48:05 -0500

    Arreglando caja con login

--- src/app/caja/tabs-caja/tabs-caja.component.ts
+++ src/app/caja/tabs-caja/tabs-caja.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { ServicesService } from '../services/services.service';
 
 @Component({
   selector: 'app-tabs-caja',
@@ -7,8 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsCajaComponent implements OnInit {
 
-  constructor() { }
+  nroCaja: String = "";
+  idIpress: String = "";
+  constructor(
+    private router: Router,
+    private cajaService: ServicesService,
+  ) {
+    this.nroCaja = JSON.parse(localStorage.getItem('cajaActual'));
+    this.idIpress = JSON.parse(localStorage.getItem('usuario')).ipress.idIpress;
+  }
 
+  clickCerrarCaja() {
+    console.log("asdasdasddddddddddddddaasd");
+    let datos = {
+      idIpress: this.idIpress,
+      ambienteCaja: this.nroCaja,
+    }
+    console.log(datos);
+    this.cajaService.cerrarCajaDiario(datos).subscribe((res: any) => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Caja',
+        text: "Caja cerrada correctamente",
+        showConfirmButton: false,
+        timer: 1500,
+      })
+      this.router.navigate(['/dashboard/caja/abrir-caja'])
+    })
+  }
   ngOnInit(): void {
   }
 
