31a0708ea278cb971cb28a62113767e877744aa5
christian <jc.yunguridata@gmail.com>
2022-06-03 16:24:16 -0500

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
     production: false,
     bd: "hce",
 
-     //baseUrl: "http://192.168.5.3:3012/api", //privada
-     baseUrl: "http://190.108.93.145:3012/api", //publica
+    //baseUrl: "http://192.168.5.3:3012/api", //privada
+    baseUrl: "http://190.108.93.145:3012/api", //publica
 
 
     /**ACCESO AL LOGIN**/
