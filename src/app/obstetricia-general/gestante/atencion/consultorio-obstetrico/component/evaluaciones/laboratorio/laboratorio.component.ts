c5b49a9f1d0fae36444b309e4d1b5c3d69fda5c3
Jonathan <jona.ml1011@gmail.com>
2022-06-15 09:43:47 -0500

    mas cambios en labo

--- src/app/obstetricia-general/gestante/atencion/consultorio-obstetrico/component/evaluaciones/laboratorio/laboratorio.component.ts
+++ src/app/obstetricia-general/gestante/atencion/consultorio-obstetrico/component/evaluaciones/laboratorio/laboratorio.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
-import {LabSolicitudComponent} from "./lab-solicitud/lab-solicitud.component";
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { DialogService, DynamicDialogRef } from "primeng/dynamicdialog";
+import { LabSolicitudComponent } from "./lab-solicitud/lab-solicitud.component";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
     selector: 'app-laboratorio',
@@ -11,10 +11,13 @@ import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 })
 export class LaboratorioComponent implements OnInit {
     ref: DynamicDialogRef;
-
+    dataConsulta: any;
     constructor(public dialog: DialogService,
-                private form: FormBuilder) {
+        private form: FormBuilder) {
+        this.dataConsulta = JSON.parse(localStorage.getItem('datosConsultaActual'));
     }
+
+
 
     ngOnInit(): void {
 
