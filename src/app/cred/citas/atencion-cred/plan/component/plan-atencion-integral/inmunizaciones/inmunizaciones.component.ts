9e42857c28aace2e1787f9db02728fa0f0cc2aef
Ansony Huallpa Mirano <ansonyhuallpam@gmail.com>
2022-08-01 12:55:26 -0500

    inmunizaciones.ts:agregamos espere porfavor

--- src/app/cred/citas/atencion-cred/plan/component/plan-atencion-integral/inmunizaciones/inmunizaciones.component.ts
+++ src/app/cred/citas/atencion-cred/plan/component/plan-atencion-integral/inmunizaciones/inmunizaciones.component.ts
@@ -3,8 +3,9 @@ import { InmunizacionesService } from "../services/inmunizaciones/inmunizaciones
 // import { Inmunizaciones } from "../models/plan-atencion-integral.model";
 import { MessageService } from "primeng/api";
 import { ActivatedRoute } from "@angular/router";
-import {inmunizaciones} from "../models/plan-atencion-integral.model";
-import {dato} from "../../../../../models/data";
+import { inmunizaciones } from "../models/plan-atencion-integral.model";
+import { dato } from "../../../../../models/data";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-inmunizaciones",
@@ -14,47 +15,46 @@ import {dato} from "../../../../../models/data";
 export class InmunizacionesComponent implements OnInit {
   valor: string = "";
   tipoDNI: string;
-  data:dato
+  data: dato;
   nroDNI: string;
   stateOptions: any[];
   listaInmunizaciones: inmunizaciones[] = [];
   // listaMeses: number[] = [0, 2, 4, 6, 7, 12, 15, 18, 96];
-  inmunizacionesAgrupadas = [[], [], [], [], [], [], [], [], [],[]];
+  inmunizacionesAgrupadas = [[], [], [], [], [], [], [], [], [], []];
   agrupaciones: any[] = [
     { abreviado: "RN", completo: "Recien Nacido" },
     { abreviado: "Menor_1A", completo: "Menor de un Año" },
     { abreviado: "1A", completo: "Un Año" },
     { abreviado: "4A", completo: "Cuatro Años" },
   ];
-  mesActual:number;
-
+  mesActual: number;
 
   constructor(
     private inmunizacionesService: InmunizacionesService,
     private messageService: MessageService,
-    private rutaActiva: ActivatedRoute,
+    private rutaActiva: ActivatedRoute
   ) {
-    this.data = <dato>JSON.parse(localStorage.getItem('documento'));
+    this.data = <dato>JSON.parse(localStorage.getItem("documento"));
     // this.mesActual=this.data.anio*12+this.data.mes;
-    this.mesActual=18;
+    this.mesActual = 18;
     // console.log(this.mesActual)
   }
-  ngOnInit(){
+  ngOnInit() {
     this.nroDNI = this.data.nroDocumento;
     this.getListaInmunizaciones();
   }
   toDate() {
-    this.listaInmunizaciones.sort((a,b)=>{
-      if (a.fechaTentativa>b.fechaTentativa)
-        return 1;
-      if (a.fechaTentativa<b.fechaTentativa)
-        return -1;
+    this.listaInmunizaciones.sort((a, b) => {
+      if (a.fechaTentativa > b.fechaTentativa) return 1;
+      if (a.fechaTentativa < b.fechaTentativa) return -1;
       return 0;
-    })
+    });
     this.listaInmunizaciones.forEach((element) => {
       element.fechaTentativa = new Date(`${element.fechaTentativa} 00:00:00`);
       element.fechaAdministracion =
-        element.fechaAdministracion != null ? new Date(`${element.fechaAdministracion} 00:00:00`) : null;
+        element.fechaAdministracion != null
+          ? new Date(`${element.fechaAdministracion} 00:00:00`)
+          : null;
     });
   }
   nombreVacuna(nombre: string) {
@@ -62,13 +62,13 @@ export class InmunizacionesComponent implements OnInit {
   }
   edadMes: number[] = [];
   clasificamos() {
-    console.log('toda la lista ordenada',this.listaInmunizaciones)
+    console.log("toda la lista ordenada", this.listaInmunizaciones);
     //['RN', 'Menor_1A', '1A', '4A'][0,1,2,4,6,7,12,15,18,96]
     this.listaInmunizaciones.forEach((element) => {
       let isInclude = this.edadMes.find((elemento) => {
         return elemento == element.edadMes;
       });
-      if (isInclude==null) {
+      if (isInclude == null) {
         this.edadMes.push(element.edadMes);
       }
     });
@@ -80,14 +80,31 @@ export class InmunizacionesComponent implements OnInit {
     });
   }
   getListaInmunizaciones() {
+    this.cargarDatosPruebaTomada();
     this.inmunizacionesService
       .getListaInmunizaciones(this.nroDNI)
       .subscribe((resp) => {
-        this.inmunizacionesAgrupadas = [[], [], [], [], [], [], [], [], [],[]];
+        setTimeout(() => {Swal.close();}, 500);
+        this.inmunizacionesAgrupadas = [[], [], [], [], [], [], [], [], [], []];
         this.listaInmunizaciones = resp["object"];
         this.toDate();
         this.clasificamos();
-
       });
+  }
+  cargarDatosPruebaTomada() {
+    Swal.fire({
+      title: "<strong>Cargando Datos</strong>",
+      html:
+        "<div style=height:6rem> <div style=font-family:Spartan; >" +
+        "Espere un momento" +
+        "</div>" +
+        "</br>" +
+        '<i class="pi pi-spin pi-spinner" style="font-size:2rem;height:2rem;width:2rem "></i> </div>',
+      showCancelButton: false,
+      showConfirmButton: false,
+      position: "top",
+      backdrop: `rgba(0,0,0,0.85) left top no-repeat`,
+      allowOutsideClick: false,
+    });
   }
 }
