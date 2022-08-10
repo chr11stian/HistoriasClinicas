9cc9c4859daf576d55356c60524d076a5be668a0
Jonathan <jona.ml1011@gmail.com>
2022-08-10 16:27:05 -0500

    loading spinner to http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ConfirmationService, MessageService, PrimeNGConfig} from 'primeng/api';
+import { ConfirmationService, MessageService, PrimeNGConfig } from 'primeng/api';
+import { SpinnerHandlerService } from './core/services/spinner-handler.service';
 
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    providers: [ConfirmationService,MessageService]
+    providers: [ConfirmationService, MessageService]
 })
 export class AppComponent implements OnInit {
-    constructor(private config: PrimeNGConfig) { }
+    loading$ = this.spinnerHandler.showSpinner$;
+    constructor(private config: PrimeNGConfig, public spinnerHandler: SpinnerHandlerService) {
+        this.openLoading();
+    }
 
     ngOnInit() {
         this.config.setTranslation({
@@ -20,5 +24,10 @@ export class AppComponent implements OnInit {
             clear: 'Limpiar',
             monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"],
         });
+        
+    }
+    openLoading() {
+        console.log('data de app');
+        console.log('data de app ', this.loading$);
     }
 }
---
+++ src/app/core/services/spinner-handler.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpinnerHandlerService {
+  // public reqNumber: number = 0;
+  private showSpinner: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public readonly showSpinner$ = this.showSpinner.asObservable();
+
+  // handleRequest = (state: string = 'minus') => {
+  //   this.reqNumber = (state === 'plus') ? this.reqNumber + 1 : this.reqNumber - 1;
+  //   this.showSpinner.next(this.reqNumber > 0);
+  // }
+
+  constructor() { }
+  show() {
+    this.showSpinner.next(true);
+    // Swal.fire('Please wait')
+    // Swal.showLoading();
+  }
+  hide() {
+    this.showSpinner.next(  false);
+    // Swal.close();
+  }
+
+}
