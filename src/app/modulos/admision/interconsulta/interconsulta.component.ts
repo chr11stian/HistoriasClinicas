c0bdf2f90052110619f9fc4488a93fe3c6632715
christian <jc.yunguridata@gmail.com>
2022-08-22 09:27:39 -0500

    interconsulta..

--- src/app/modulos/admision/interconsulta/interconsulta.component.ts
+++ src/app/modulos/admision/interconsulta/interconsulta.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { interconsulta } from "./../models/model";
+import { InterconsultaService } from "./../services/interconsulta.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-interconsulta',
-  templateUrl: './interconsulta.component.html',
-  styleUrls: ['./interconsulta.component.css']
+  selector: "app-interconsulta",
+  templateUrl: "./interconsulta.component.html",
+  styleUrls: ["./interconsulta.component.css"],
 })
 export class InterconsultaComponent implements OnInit {
-
-  constructor() { }
+  listInterconsulta: interconsulta[] = [];
+  constructor(private serviceInterconsulta: InterconsultaService) {}
 
   ngOnInit(): void {
+    this.serviceInterconsulta.listInterconsulta().subscribe((r: any) => {
+      this.listInterconsulta = r.object;
+      console.log(this.listInterconsulta)
+    });
   }
-
 }
