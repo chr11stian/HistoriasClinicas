684d87d5f9b5c89798aec8ddda1ad73ac7c3c77d
Jonathan <jona.ml1011@gmail.com>
2022-08-26 17:09:14 -0500

    condicionales para validar los tabs de desarrollo psicomotor

--- src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/components/desarrollo-psicomotor/desarrollo-psicomotor.component.ts
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/evaluaciones-consulta/components/desarrollo-psicomotor/desarrollo-psicomotor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DesarrolloPsicomotorService } from './services/desarrollo-psicomotor.service';
 
 @Component({
   selector: 'app-desarrollo-psicomotor',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesarrolloPsicomotorComponent implements OnInit {
 
-  constructor() { }
+  paciente: any;
+  monthAge: number;
+  index: number = 0;
+  disabledSelectedTab: DisabledSelected[] = [{ disabled: true, selected: false }, { disabled: false, selected: true }, { disabled: true, selected: false }, { disabled: true, selected: false }];
+  evaluations: any;
 
-  ngOnInit(): void {
+
+  constructor(
+    private evalPsicomotor: DesarrolloPsicomotorService,
+  ) {
+    this.paciente = JSON.parse(localStorage.getItem('documento'));
+    this.monthAge = this.overallAge(this.paciente.anio, this.paciente.mes);
   }
 
+  ngOnInit(): void {
+    this.searchMonthlyEvaluation();
+  }
+
+  searchMonthlyEvaluation(): void {
+    this.evalPsicomotor.verifyMonthlyEvaluation(this.monthAge, this.paciente.nroDocumento).then(res => {
+      this.evaluations = res[0];
+      console.log('res de evaluation ', this.evaluations);
+      // if (this.evaluations[]) {
+
+      // }
+    })
+  }
+
+  overallAge(year: number, month: number): number {
+    return 12 * year + month;
+  }
+
+  selectedTab() {
+    console.log('opcion ', this.index);
+  }
+}
+
+interface DisabledSelected {
+  disabled: boolean,
+  selected: boolean
 }
