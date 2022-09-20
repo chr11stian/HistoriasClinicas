8a5f249be56f03c184115836a43c4ddf21024e2d
Jonathan <jona.ml1011@gmail.com>
2022-09-20 09:06:25 -0500

    imagen para pacientes sin fotos en triaje

--- src/app/modulos/triaje/registrar-triaje/registrar-triaje.component.ts
+++ src/app/modulos/triaje/registrar-triaje/registrar-triaje.component.ts
@@ -1,16 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
-import {MessageService} from 'primeng/api';
-import {DialogService, DynamicDialogConfig, DynamicDialogRef} from 'primeng/dynamicdialog';
-import {ConsultasService} from 'src/app/obstetricia-general/gestante/atencion/consultorio-obstetrico/services/consultas.service';
-import {ConsultaObstetriciaService} from 'src/app/obstetricia-general/gestante/consulta/services/consulta-obstetricia/consulta-obstetricia.service';
-import {CuposTriajeService} from '../services/cupos-triaje/cupos-triaje.service';
-import {image} from '../../../../assets/images/image.const';
-import {imageNina} from '../../../../assets/images/imageNina.const';
-import {imageNino} from '../../../../assets/images/imageNino.const';
-import {PersonalService} from 'src/app/core/services/personal-services/personal.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ConsultasService } from 'src/app/obstetricia-general/gestante/atencion/consultorio-obstetrico/services/consultas.service';
+import { ConsultaObstetriciaService } from 'src/app/obstetricia-general/gestante/consulta/services/consulta-obstetricia/consulta-obstetricia.service';
+import { CuposTriajeService } from '../services/cupos-triaje/cupos-triaje.service';
+import { image } from '../../../../assets/images/image.const';
+import { imageNina } from '../../../../assets/images/imageNina.const';
+import { imageNino } from '../../../../assets/images/imageNino.const';
+import { imagePerson } from 'src/assets/images/imagePerson.const';
+import { PersonalService } from 'src/app/core/services/personal-services/personal.service';
 import Swal from "sweetalert2";
-import {DatePipe} from "@angular/common";
+import { DatePipe } from "@angular/common";
 
 @Component({
     selector: 'app-registrar-triaje',
@@ -47,7 +48,7 @@ export class RegistrarTriajeComponent implements OnInit {
         private messageService: MessageService,
     ) {
         this.inicializarForm();
-        console.log('data of listar ', config.data.data);
+        // console.log('data of listar ', config.data.data);
         this.datosPersonales = config.data.data;
         this.idCupo = this.datosPersonales.id;
         if (this.datosPersonales.paciente.edadAnio < 18) {
@@ -60,13 +61,13 @@ export class RegistrarTriajeComponent implements OnInit {
         }
         if (config.data.option == 2) {
             this.ver = true;
-            this.formTriaje.patchValue({temperatura: this.datosPersonales.funcionesVitales.temperatura});
-            this.formTriaje.patchValue({presionSis: this.datosPersonales.funcionesVitales.presionSistolica});
-            this.formTriaje.patchValue({presionDias: this.datosPersonales.funcionesVitales.presionDiastolica});
-            this.formTriaje.patchValue({fc: this.datosPersonales.funcionesVitales.fc});
-            this.formTriaje.patchValue({fr: this.datosPersonales.funcionesVitales.fr});
-            this.formTriaje.patchValue({peso: this.datosPersonales.funcionesVitales.peso});
-            this.formTriaje.patchValue({talla: this.datosPersonales.funcionesVitales.talla});
+            this.formTriaje.patchValue({ temperatura: this.datosPersonales.funcionesVitales.temperatura });
+            this.formTriaje.patchValue({ presionSis: this.datosPersonales.funcionesVitales.presionSistolica });
+            this.formTriaje.patchValue({ presionDias: this.datosPersonales.funcionesVitales.presionDiastolica });
+            this.formTriaje.patchValue({ fc: this.datosPersonales.funcionesVitales.fc });
+            this.formTriaje.patchValue({ fr: this.datosPersonales.funcionesVitales.fr });
+            this.formTriaje.patchValue({ peso: this.datosPersonales.funcionesVitales.peso });
+            this.formTriaje.patchValue({ talla: this.datosPersonales.funcionesVitales.talla });
             this.calcularIMC();
             this.formTriaje.get("temperatura").disable();
             this.formTriaje.get("presionSis").disable();
@@ -77,59 +78,59 @@ export class RegistrarTriajeComponent implements OnInit {
             this.formTriaje.get("talla").disable();
         }
         if (config.data.option == 3) {
-            this.formTriaje.patchValue({temperatura: this.datosPersonales.funcionesVitales.temperatura});
-            this.formTriaje.patchValue({presionSis: this.datosPersonales.funcionesVitales.presionSistolica});
-            this.formTriaje.patchValue({presionDias: this.datosPersonales.funcionesVitales.presionDiastolica});
-            this.formTriaje.patchValue({fc: this.datosPersonales.funcionesVitales.fc});
-            this.formTriaje.patchValue({fr: this.datosPersonales.funcionesVitales.fr});
-            this.formTriaje.patchValue({peso: this.datosPersonales.funcionesVitales.peso});
-            this.formTriaje.patchValue({talla: this.datosPersonales.funcionesVitales.talla});
+            this.formTriaje.patchValue({ temperatura: this.datosPersonales.funcionesVitales.temperatura });
+            this.formTriaje.patchValue({ presionSis: this.datosPersonales.funcionesVitales.presionSistolica });
+            this.formTriaje.patchValue({ presionDias: this.datosPersonales.funcionesVitales.presionDiastolica });
+            this.formTriaje.patchValue({ fc: this.datosPersonales.funcionesVitales.fc });
+            this.formTriaje.patchValue({ fr: this.datosPersonales.funcionesVitales.fr });
+            this.formTriaje.patchValue({ peso: this.datosPersonales.funcionesVitales.peso });
+            this.formTriaje.patchValue({ talla: this.datosPersonales.funcionesVitales.talla });
             this.calcularIMC();
         }
     }
 
-    onChangeTemperatura(){
-        let temp=parseFloat(this.formTriaje.value.temperatura);
-        if (temp<35){   
-            this.alertaTbaja=true
-            this.alertaTalta=false
+    onChangeTemperatura() {
+        let temp = parseFloat(this.formTriaje.value.temperatura);
+        if (temp < 35) {
+            this.alertaTbaja = true
+            this.alertaTalta = false
         }
-        else  {
-            if (temp>=37.5){
-                this.alertaTbaja=false
-                this.alertaTalta=true
+        else {
+            if (temp >= 37.5) {
+                this.alertaTbaja = false
+                this.alertaTalta = true
             }
             else {
-                this.alertaTbaja=false
-                this.alertaTalta=false
+                this.alertaTbaja = false
+                this.alertaTalta = false
             }
         }
     }
-    onChangePAs(){
-        let temp=parseFloat(this.formTriaje.value.presionSis);
-        if (temp>=140) this.alertaPAs=true
-        else this.alertaPAs=false
+    onChangePAs() {
+        let temp = parseFloat(this.formTriaje.value.presionSis);
+        if (temp >= 140) this.alertaPAs = true
+        else this.alertaPAs = false
     }
-    onChangePAd(){
-        let temp=parseFloat(this.formTriaje.value.presionDias);
-        if (temp>90) this.alertaPAd=true
-        else this.alertaPAd=false
+    onChangePAd() {
+        let temp = parseFloat(this.formTriaje.value.presionDias);
+        if (temp > 90) this.alertaPAd = true
+        else this.alertaPAd = false
     }
-    onChangeFR(){
-        let temp=parseFloat(this.formTriaje.value.fr);
-        if (temp>100) this.alertaFR=true
-        else this.alertaFR=false
+    onChangeFR() {
+        let temp = parseFloat(this.formTriaje.value.fr);
+        if (temp > 100) this.alertaFR = true
+        else this.alertaFR = false
     }
-    onChangeFC(){
-        let temp=parseFloat(this.formTriaje.value.fc);
-        if (temp>22) this.alertaFC=true
-        else this.alertaFC=false
+    onChangeFC() {
+        let temp = parseFloat(this.formTriaje.value.fc);
+        if (temp > 22) this.alertaFC = true
+        else this.alertaFC = false
     }
     traerFoto() {
         this.personalService.getDatosReniec(this.datosPersonales.paciente.nroDoc).subscribe((res: any) => {
             this.dataPIDE = res;
-            console.log(res);
-            this.imagePath = res.foto;
+            // console.log(res);
+            res.foto == null ? this.imagePath = imagePerson : this.imagePath = res.foto;
         });
     }
 
@@ -163,7 +164,7 @@ export class RegistrarTriajeComponent implements OnInit {
 
     guardarTriaje() {
         this.recuperarDatos();
-        console.log('data to show ', this.triaje);
+        // console.log('data to show ', this.triaje);
         this.triajeService.postTriaje(this.idCupo, this.triaje).subscribe((res: any) => {
             Swal.fire({
                 icon: 'success',
@@ -206,13 +207,13 @@ export class RegistrarTriajeComponent implements OnInit {
     }
 
     loadData() {
-        this.formTriaje.patchValue({temperatura: ''});
-        this.formTriaje.patchValue({presionSis: ''});
-        this.formTriaje.patchValue({presionDias: ''});
-        this.formTriaje.patchValue({fc: ''});
-        this.formTriaje.patchValue({fr: ''});
-        this.formTriaje.patchValue({peso: ''});
-        this.formTriaje.patchValue({talla: ''});
+        this.formTriaje.patchValue({ temperatura: '' });
+        this.formTriaje.patchValue({ presionSis: '' });
+        this.formTriaje.patchValue({ presionDias: '' });
+        this.formTriaje.patchValue({ fc: '' });
+        this.formTriaje.patchValue({ fr: '' });
+        this.formTriaje.patchValue({ peso: '' });
+        this.formTriaje.patchValue({ talla: '' });
     }
 
     calcularIMC() {
