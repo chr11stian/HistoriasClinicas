f08b4c0e3d12100f046980ec9ec0a46ab67d104d
Carlos Alberto Borda Benito <javadebian@gmail.com>
2022-09-22 16:50:26 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,8 @@
 export const environment = {
     production: false,
     bd: "hce",
+    bdCouch:'visitas_medicas',
+    
     //baseUrl: "http://192.168.5.3:3012/api", //privada
     baseUrl: "http://190.108.93.150:3012/api", //publica
 
