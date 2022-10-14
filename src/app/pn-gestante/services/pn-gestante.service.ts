f383e0412f96c796bdc44840fd8bc3d2f8433765
christian <jc.yunguridata@gmail.com>
2022-10-14 17:42:57 -0500

    cambios yuvisa

--- src/app/pn-gestante/services/pn-gestante.service.ts
+++ src/app/pn-gestante/services/pn-gestante.service.ts
@@ -1,151 +1,159 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { PNGestante } from '../interfaces/pn-gestante';
-import { GestanteModel } from '../interfaces/GestanteModel';
-import { NuevaGesta } from '../interfaces/NuevaGesta';
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { PNGestante } from "../interfaces/pn-gestante";
+import { GestanteModel } from "../interfaces/GestanteModel";
+import { NuevaGesta } from "../interfaces/NuevaGesta";
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class PnGestanteService {
-  couch: boolean = false;
-  bd = environment.bd_pn_gestante;
-  base_url = environment.base_url_Couch;
-  base_url_view = environment.base_url_couch_pngestante_view;
-  base_url_update=environment.base_url_couch_gestante_update;
-  auxNroDocPersonal: string = JSON.parse(localStorage.getItem("usuario"))
-    .nroDocumento;
-  auxNombresPersonal: string = JSON.parse(localStorage.getItem("usuario"))
-    .nombres;
-  auxApellidosPersonal: string = JSON.parse(localStorage.getItem("usuario"))
-    .apellidos;
-  auxCodeessActual: string = JSON.parse(localStorage.getItem("usuario")).ipress
-    .renipress;
-  aux_eessActual: string = JSON.parse(localStorage.getItem("usuario")).ipress
-    .nombreEESS;
+    couch: boolean = false;
+    bd = environment.bd_pn_gestante;
+    base_url = environment.base_url_Couch;
+    base_url_view = environment.base_url_couch_pngestante_view;
+    base_url_update = environment.base_url_couch_gestante_update;
 
-  getauxNroDocPersonal():string{
-    return this.auxNroDocPersonal;
-  }
-
-  getauxNombresPersonal():string{
-    return this.auxNombresPersonal;
-  }
-
-  getauxApellidosPersonal():string{
-    return this.auxApellidosPersonal;
-  }
-  constructor(private http: HttpClient) {
-  }
-
-  getauxCodeessActual():string{
-    return this.auxCodeessActual;
-  }
-
-  getaux_eessActual():string{
-    return this.aux_eessActual;
-  }
-
-  getToken() {
-    return JSON.parse(localStorage.getItem("token")).tokenCouch === null
-        ? ""
-        : JSON.parse(localStorage.getItem("token")).tokenCouch;
-}
-
-getAnio(): string {
-    let fecha_hoy = new Date();
-    let anio = fecha_hoy.getFullYear();
-    return anio.toString();
-}
-
-mostrarPadronGestantes(cod_ipress:string){
-    return this.http.post<any []>(
-      `${this.base_url_view}/pn-gestantes-ipress`,
-      {
-        "keys": [`${cod_ipress}`],
-      })
+    getauxNroDocPersonal(): string {
+        return JSON.parse(localStorage.getItem("usuario")).nroDocumento === null
+            ? ""
+            : JSON.parse(localStorage.getItem("usuario")).nroDocumento;
     }
 
-  addGestante(pn_gestante:GestanteModel) {
-    return this.http.post(`${this.base_url}/${this.bd}`, pn_gestante);
-  }
+    getauxNombresPersonal(): string {
+        return JSON.parse(localStorage.getItem("usuario")).nombres === null
+            ? ""
+            : JSON.parse(localStorage.getItem("usuario")).nombres;
+    }
 
-  updatedGestante(
-    id: string,
-    updatedPnGestante:any,
-    revision: string
-  ) {
-    return this.http.put(`${this.base_url}/${this.bd}/${id}`, updatedPnGestante, {
-      params: {
-        rev: revision,
-      },
-    });
-  }
+    getauxApellidosPersonal(): string {
+        return JSON.parse(localStorage.getItem("usuario")).apellidos === null
+            ? ""
+            : JSON.parse(localStorage.getItem("usuario")).apellidos;
+    }
+    constructor(private http: HttpClient) {}
 
-  getGestanteDni(dni:String){
-    //implementar una vista para traer gestante del padron gestantes_dni
-    return this.http.post<any []>(
-      `${this.base_url_view}/gestantes-dni`,
-      {
-        "keys": [`${dni}`],
-      })
-  }
+    getauxCodeessActual(): string {
+        return JSON.parse(localStorage.getItem("usuario")).ipress.renipress ===
+            null
+            ? ""
+            : JSON.parse(localStorage.getItem("usuario")).ipress.renipress;
+    }
 
-  cambioEESS(id:string,nuevo_codEessActual:string,nuevo_eessActual:string){
-    console.log(`${this.base_url_update}/cambiar_eess/id`);
-    return this.http.put(
-      `${this.base_url_update}/cambiar_eess/${id}`,
-      {
-        codEessActual:nuevo_codEessActual,
-        eessActual:nuevo_eessActual
-      })
-  }
+    getaux_eessActual(): string {
+        return JSON.parse(localStorage.getItem("usuario")).ipress.nombreEESS ===
+            null
+            ? ""
+            : JSON.parse(localStorage.getItem("usuario")).ipress.nombreEESS;
+    }
 
-  actualizarNumeroGesta(id:string,nuevo_nroGesta:NuevaGesta,nuevo_fur:string,nuevo_fpp:string){
-    console.log( `${this.base_url_update}/actualizar_nroGesta/${id}`);
-    return this.http.put(
-      `${this.base_url_update}/actualizar_nroGesta/${id}`,
-      {
-        nroGesta:{
-          nroGesta:nuevo_nroGesta['nroGesta'],
-          fur:nuevo_nroGesta['fur'],
-          fpp:nuevo_nroGesta['fpp'],
-          codEessActual:nuevo_nroGesta['codEessActual'],
-          eessActual:nuevo_nroGesta['eessActual']
-        },
-        fur:nuevo_fur,
-        fpp:nuevo_fpp
-      })
-  }
+    getToken() {
+        return JSON.parse(localStorage.getItem("token")).tokenCouch === null
+            ? ""
+            : JSON.parse(localStorage.getItem("token")).tokenCouch;
+    }
 
-  actualizarInformacionGestante(id:string,cambio_gestante:GestanteModel){
-    console.log( `${this.base_url_update}/actualizar_dataGestante/${id}`);
-    return this.http.put(
-      `${this.base_url_update}/actualizar_dataGestante/${id}`,
-      {
-        "nombres":cambio_gestante["nombres"],
-        "apellidos": cambio_gestante["apellidos"],
-        "fechaNacimiento": cambio_gestante["fechaNacimiento"],
-        "tipoDocIdentidad": cambio_gestante["tipoDocIdentidad"],
-        "nroDocIdentidad": cambio_gestante["nroDocIdentidad"],
-        "telefono": cambio_gestante["telefono"],
-        "tieneSis": cambio_gestante["tieneSis"],
-        "direccion": cambio_gestante["direccion"],
-        "referencia": cambio_gestante["referencia"],
-        "hcl2": cambio_gestante["hcl2"],
-        "codEessAnterior": cambio_gestante["codEessAnterior"],
-        "eessAnterior": cambio_gestante["eessAnterior"],
-        "codEessActual": cambio_gestante["codEessActual"],
-        "eessActual": cambio_gestante["eessActual"],
-        "fur":cambio_gestante["fur"],
-        "fpp": cambio_gestante["fpp"],
-        "morbilidadPotencial": cambio_gestante["morbilidadPotencial"],
-        "observaciones": cambio_gestante["observaciones"],
-        "dniPersonal": cambio_gestante["dniPersonal"],
-        "personalEess": cambio_gestante["personalEess"],
-        "fechaReg": cambio_gestante["fechaReg"],
-        "aborto": cambio_gestante["aborto"],
-      })
-  }
+    getAnio(): string {
+        let fecha_hoy = new Date();
+        let anio = fecha_hoy.getFullYear();
+        return anio.toString();
+    }
 
+    mostrarPadronGestantes(cod_ipress: string) {
+        return this.http.post<any[]>(
+            `${this.base_url_view}/pn-gestantes-ipress`,
+            {
+                keys: [`${cod_ipress}`],
+            }
+        );
+    }
+
+    addGestante(pn_gestante: GestanteModel) {
+        return this.http.post(`${this.base_url}/${this.bd}`, pn_gestante);
+    }
+
+    updatedGestante(id: string, updatedPnGestante: any, revision: string) {
+        return this.http.put(
+            `${this.base_url}/${this.bd}/${id}`,
+            updatedPnGestante,
+            {
+                params: {
+                    rev: revision,
+                },
+            }
+        );
+    }
+
+    getGestanteDni(dni: String) {
+        //implementar una vista para traer gestante del padron gestantes_dni
+        return this.http.post<any[]>(`${this.base_url_view}/gestantes-dni`, {
+            keys: [`${dni}`],
+        });
+    }
+
+    cambioEESS(
+        id: string,
+        nuevo_codEessActual: string,
+        nuevo_eessActual: string
+    ) {
+        console.log(`${this.base_url_update}/cambiar_eess/id`);
+        return this.http.put(`${this.base_url_update}/cambiar_eess/${id}`, {
+            codEessActual: nuevo_codEessActual,
+            eessActual: nuevo_eessActual,
+        });
+    }
+
+    actualizarNumeroGesta(
+        id: string,
+        nuevo_nroGesta: NuevaGesta,
+        nuevo_fur: string,
+        nuevo_fpp: string
+    ) {
+        console.log(`${this.base_url_update}/actualizar_nroGesta/${id}`);
+        return this.http.put(
+            `${this.base_url_update}/actualizar_nroGesta/${id}`,
+            {
+                nroGesta: {
+                    nroGesta: nuevo_nroGesta["nroGesta"],
+                    fur: nuevo_nroGesta["fur"],
+                    fpp: nuevo_nroGesta["fpp"],
+                    codEessActual: nuevo_nroGesta["codEessActual"],
+                    eessActual: nuevo_nroGesta["eessActual"],
+                },
+                fur: nuevo_fur,
+                fpp: nuevo_fpp,
+            }
+        );
+    }
+
+    actualizarInformacionGestante(id: string, cambio_gestante: GestanteModel) {
+        console.log(`${this.base_url_update}/actualizar_dataGestante/${id}`);
+        return this.http.put(
+            `${this.base_url_update}/actualizar_dataGestante/${id}`,
+            {
+                nombres: cambio_gestante["nombres"],
+                apellidos: cambio_gestante["apellidos"],
+                fechaNacimiento: cambio_gestante["fechaNacimiento"],
+                tipoDocIdentidad: cambio_gestante["tipoDocIdentidad"],
+                nroDocIdentidad: cambio_gestante["nroDocIdentidad"],
+                telefono: cambio_gestante["telefono"],
+                tieneSis: cambio_gestante["tieneSis"],
+                direccion: cambio_gestante["direccion"],
+                referencia: cambio_gestante["referencia"],
+                hcl2: cambio_gestante["hcl2"],
+                codEessAnterior: cambio_gestante["codEessAnterior"],
+                eessAnterior: cambio_gestante["eessAnterior"],
+                codEessActual: cambio_gestante["codEessActual"],
+                eessActual: cambio_gestante["eessActual"],
+                fur: cambio_gestante["fur"],
+                fpp: cambio_gestante["fpp"],
+                morbilidadPotencial: cambio_gestante["morbilidadPotencial"],
+                observaciones: cambio_gestante["observaciones"],
+                dniPersonal: cambio_gestante["dniPersonal"],
+                personalEess: cambio_gestante["personalEess"],
+                fechaReg: cambio_gestante["fechaReg"],
+                aborto: cambio_gestante["aborto"],
+            }
+        );
+    }
 }
