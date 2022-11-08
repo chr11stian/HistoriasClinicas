6b5f63c7a51a3fc33a9df2faf18605a96cadf1f4
christian <jc.yunguridata@gmail.com>
2022-11-08 12:43:43 -0500

    guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,50 @@
-import {NgModule} from "@angular/core";
-import {Routes, RouterModule, PreloadAllModules} from "@angular/router";
-import {TopMenuComponent} from "./core/top-menu/top-menu.component";
-import {LoginComponent} from './login/login.component';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {AuthGuard} from './core/guards/authGuard';
-import {LoginRolComponent} from "./login/login-rol/login-rol.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
+import { TopMenuComponent } from "./core/top-menu/top-menu.component";
+import { LoginComponent } from "./login/login.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { AuthGuard } from "./core/guards/authGuard";
 
 const routes: Routes = [
     {
         path: "",
         redirectTo: "login",
-        pathMatch: "full"
+        pathMatch: "full",
     },
     {
         path: "login",
-        component: LoginComponent
+        component: LoginComponent,
     },
     {
         path: "dashboard",
-        loadChildren: () => import('src/app/core/core.module').then(n => n.CoreModule),
-        //canActivate: [AuthGuard],
-        data: {roles: ["GERESA", "RED", "MICRORED", "IPRESS"]}
+        loadChildren: () =>
+            import("src/app/core/core.module").then((n) => n.CoreModule),
+        canActivate: [AuthGuard],
+        data: {
+            roles: [
+                "ROLE_ADMIN",
+                "ROLE_ENF_PERSONAL",
+                "ROLE_TEC_ADMINI_PERSONAL",
+                "ISITA_DOMICILIARIA_PROFESIONAL",
+                "ROLE_TEC_ADMINI_ADMIN",
+                "VISITA_DOMICILIARIA_ACTOR_SOCIAL",
+                "ROLE_FARM_PERSONAL",
+                "ROLE_LAB_PERSONAL",
+            ],
+        },
     },
     {
         path: "menu",
-        component: TopMenuComponent
+        component: TopMenuComponent,
     },
     {
         path: "**",
-        component: NotFoundComponent
-    }
+        component: NotFoundComponent,
+    },
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes,{onSameUrlNavigation: 'reload'})
-    ],
-    exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: "reload" })],
+    exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
