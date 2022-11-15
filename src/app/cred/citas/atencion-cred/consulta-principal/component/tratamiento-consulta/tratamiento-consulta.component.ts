28331c0f471f430622c7d650d5cec3a088316325
Ansony Huallpa Mirano <ansonyhuallpam@gmail.com>
2022-11-15 09:01:42 -0500

    tratamiento:kick log

--- src/app/cred/citas/atencion-cred/consulta-principal/component/tratamiento-consulta/tratamiento-consulta.component.ts
+++ src/app/cred/citas/atencion-cred/consulta-principal/component/tratamiento-consulta/tratamiento-consulta.component.ts
@@ -35,13 +35,6 @@ export class TratamientoConsultaComponent implements OnInit {
     fecha: Date;
     servicios: string[] = [];
     loading: boolean = false;
-    urgencia = [
-        { name: "Nivel 1", code: "Nivel 1" },
-        { name: "Nivel 2", code: "Nivel 2" },
-        { name: "Nivel 3", code: "Nivel 3" },
-        { name: "Nivel 4", code: "Nivel 4" },
-        { name: "Nivel 5", code: "Nivel 5" },
-    ];
     //--HIS
     dialogHIS: boolean;
     isUpdateHIS: boolean = false;
@@ -58,160 +51,20 @@ export class TratamientoConsultaComponent implements OnInit {
         private finalizarConsulta: FinalizarConsultaService,
         private router: Router,
     ) {
-        this.build();
         // this.nexDate = this.consultaGeneralService.fecha
         // console.log("proxima citaaaa ", this.nexDate);
-    }
-    build() {
-        /* Interconsulta */
-        this.formInterconsulta = new FormGroup({
-            fecha: new FormControl({ value: null, disabled: false }, []),
-            motivo: new FormControl({ value: "", disabled: false }, []),
-            servicio: new FormControl({ value: "", disabled: false }, []),
-            urgencia: new FormControl({ value: "", disabled: false }, []),
-        });
     }
     ngOnInit(): void {
         this.data = <dato>(
             JSON.parse(localStorage.getItem(this.attributeLocalS))
         );
-        /* interconsulta */
-        this.ListaServicios();
-        this.tooltipItems = [
-            {
-                tooltipOptions: {
-                    tooltipLabel: "Reporte",
-                    tooltipPosition: "left",
-                },
-                icon: "pi pi-desktop",
-                command: (event: Event) => {
-                    this.open();
-                },
-            },
-            {
-                tooltipOptions: {
-                    tooltipLabel: "Reporte",
-                    tooltipPosition: "left",
-                },
-                icon: "pi pi-desktop",
-                command: (event: Event) => {
-                    this.open();
-                },
-            },
-            {
-                tooltipOptions: {
-                    tooltipLabel: "Reporte",
-                    tooltipPosition: "left",
-                },
-                icon: "pi pi-desktop",
-                command: (event: Event) => {
-                    this.open();
-                },
-            },
-            {
-                tooltipOptions: {
-                    tooltipLabel: "Reporte",
-                    tooltipPosition: "left",
-                },
-                icon: "pi pi-desktop",
-                command: (event: Event) => {
-                    this.open();
-                },
-            },
-            {
-                tooltipOptions: {
-                    tooltipLabel: "Interconsulta",
-                    tooltipPosition: "left",
-                },
-                icon: "pi pi-external-link",
-                command: (event: Event) => {
-                    this.open();
-                },
-            },
-        ];
-        /* lista interconsulta */
-        this.listaInterconsulta();
-        /* his */
-
     }
 
     /* interconsulta */
     open(): void {
         this.isUpdate = false;
-        this.formInterconsulta.reset();
-        this.formInterconsulta.get("fecha").setValue("");
-        this.formInterconsulta.get("motivo").setValue("");
-        this.formInterconsulta.get("servicio").setValue("");
-        this.formInterconsulta.get("urgencia").setValue("");
-        this.dialogInterconsulta = true;
-    }
-    ListaServicios() {
-        let idIpress = JSON.parse(localStorage.getItem("usuario")).ipress
-            .idIpress;
-        this.rolGuardiaService
-            .getServiciosPorIpress(idIpress)
-            .subscribe((res: any) => {
-                this.servicios = res.object;
-                // console.log("LISTA DE SERVICIOS DE IPRESSS", this.servicios);
-            });
     }
 
-    eliminarInterconsulta(id, index) {
-        this.listInterconsulta.splice(index, 1);
-        console.log();
-        this.consultaGeneralService
-            .deleteInterconsulta(this.data.idConsulta, id)
-            .subscribe((r: any) => {
-                console.log(r.object);
-            });
-    }
-    listaInterconsulta() {
-        this.consultaGeneralService
-            .listInterconsulta(this.data.idConsulta)
-            .subscribe((r: any) => {
-                this.listInterconsulta = r.object;
-            });
-    }
-    agregarInterconsulta() {
-        this.loading = true;
-        setTimeout(() => (this.loading = false), 1000);
-        /* agregar */
-        if (
-            this.formInterconsulta.value.fecha != null &&
-            this.formInterconsulta.value.motivo != "" &&
-            this.formInterconsulta.value.servicio != ""
-        ) {
-            let interconsulta: proxCita = {
-                fecha: this.datePipe.transform(
-                    this.formInterconsulta.value.fecha,
-                    "yyyy-MM-dd"
-                ),
-                motivo: this.formInterconsulta.value.motivo.toUpperCase(),
-                servicio: this.formInterconsulta.value.servicio,
-                nivelUrgencia: this.formInterconsulta.value.urgencia,
-            };
-            this.consultaGeneralService
-                .addInterconsulta(this.data.idConsulta, interconsulta)
-                .subscribe((r: any) => {
-                    this.listInterconsulta = r.object;
-                });
-            Swal.fire({
-                icon: "success",
-                title: "Agregado correctamente",
-                text: "",
-                showConfirmButton: false,
-                timer: 1500,
-            });
-        } else {
-            Swal.fire({
-                icon: "warning",
-                title: "Datos incompletos",
-                text: "",
-                showConfirmButton: false,
-                timer: 1500,
-            });
-        }
-    }
 
     concludeConsultation(): void {
         this.nexDate = {
